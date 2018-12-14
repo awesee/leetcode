9cd78d7e1461861057bf1aa910524770bc000b1c
Openset <openset.wang@gmail.com>
2018-12-14 16:50:38 +0800

    Add: twoSum

---
+++ README.md
@@ -0,0 +1,6 @@
+# LeetCode
+LeetCode Problems' Solutions
+
+| # | Title | Solution | Difficulty |
+| - | - | - | :-: |
+| 1 | [Two Sum](https://leetcode.com/problems/two-sum) | [Golang](solution/two-sum/twoSum.go) | Easy |
---
+++ solution/two-sum/twoSum.go
@@ -0,0 +1,14 @@
+package two_sum
+
+func twoSum(nums []int, target int) []int {
+
+	m := make(map[int]int)
+	for j, v := range nums {
+		if i, ok := m[target-v]; ok {
+			return []int{i, j}
+		}
+		m[v] = j
+	}
+
+	return nil
+}
