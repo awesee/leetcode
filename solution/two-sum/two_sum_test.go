5f97780d77ffd65853b3110226ede791c32b48c6
Openset <openset.wang@gmail.com>
2018-12-14 21:45:25 +0800

    Add: two_sum_test

---
+++ solution/two-sum/two_sum_test.go
@@ -0,0 +1,43 @@
+package two_sum
+
+import (
+	"testing"
+)
+
+type caseType struct {
+	Nums     []int
+	Target   int
+	Expected []int
+}
+
+func TestTwoSum(t *testing.T) {
+	tests := [...]caseType{
+		{
+			Nums:     []int{2, 7, 11, 15},
+			Target:   9,
+			Expected: []int{0, 1},
+		},
+		{
+			Nums:     []int{2, 7, 11, 15},
+			Target:   10,
+			Expected: nil,
+		},
+		{
+			Nums:     []int{1, 2, 3, 4},
+			Target:   5,
+			Expected: []int{1, 2},
+		},
+	}
+
+	for i, test := range tests {
+		output := twoSum(test.Nums, test.Target)
+		for k, v := range test.Expected {
+			if output[k] != v {
+				t.Fatalf(
+					"Case: %v, Input: %v, Output: %v, Expected: %v",
+					i, test.Nums, twoSum(test.Nums, test.Target), test.Expected,
+				)
+			}
+		}
+	}
+}
