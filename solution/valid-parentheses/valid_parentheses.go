58dfa99be6c65c87132acdd494fcf34a42c6f41e
Openset <openset.wang@gmail.com>
2018-12-15 13:46:35 +0800

    Update: valid_parentheses

---
+++ solution/valid-parentheses/valid_parentheses.go
@@ -0,0 +1,25 @@
+package valid_parentheses
+
+func isValid(s string) bool {
+	m := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+	stack := make([]rune, len(s))
+	top := 0
+	for _, c := range s {
+		switch c {
+		case '(', '[', '{':
+			stack[top] = m[c]
+			top++
+		case ')', ']', '}':
+			if top > 0 && stack[top-1] == c {
+				top--
+			} else {
+				return false
+			}
+		}
+	}
+	return top == 0
+}
---
+++ solution/valid-parentheses/valid_parentheses_test.go
@@ -0,0 +1,20 @@
+package valid_parentheses
+
+import "testing"
+
+func TestIsValid(t *testing.T) {
+	tests := map[string]bool{
+		"()":     true,
+		"()[]{}": true,
+		"{[]}":   true,
+		"(]":     false,
+		"([)]":   false,
+	}
+
+	for input, expected := range tests {
+		output := isValid(input)
+		if output != expected {
+			t.Fatalf("Input: %v, Output: %v, Expected: %v", input, output, expected)
+		}
+	}
+}
