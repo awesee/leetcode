a162706373f8a4b4e735f0582f37897b01abd676
Openset <openset.wang@gmail.com>
2018-12-15 11:20:19 +0800

    Add: test case

--- solution/string-to-integer-atoi/string_to_integer_atoi_test.go
+++ solution/string-to-integer-atoi/string_to_integer_atoi_test.go
@@ -1,15 +1,24 @@
 package string_to_integer_atoi
 
-import "testing"
+import (
+	"testing"
+	"math"
+)
 
 func TestMyAtoi(t *testing.T) {
 	tests := map[string]int{
+		" ":               0,
 		"42":              42,
-		"+42":             42,
-		"   -42":          -42,
+		"+123":            123,
+		"+123 +":          123,
+		"++123":           0,
+		" -42 ":           -42,
+		" -123-":          -123,
+		"--321":           0,
 		"4193 with words": 4193,
 		"words and 987":   0,
-		"-91283472332":    -2147483648,
+		"-91283472332":    math.MinInt32,
+		"9876543210":      math.MaxInt32,
 	}
 
 	for input, expected := range tests {
