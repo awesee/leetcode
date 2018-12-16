0a3486a5257469c0155de33fd2afe3de2e37dfad
Openset <openset.wang@gmail.com>
2018-12-16 17:27:11 +0800

    Add: remove_element

---
+++ solution/remove-element/remove_element.go
@@ -0,0 +1,12 @@
+package remove_element
+
+func removeElement(nums []int, val int) int {
+	l := 0
+	for _, v := range nums {
+		if v != val {
+			nums[l] = v
+			l++
+		}
+	}
+	return l
+}
---
+++ solution/remove-element/remove_element_test.go
@@ -0,0 +1,56 @@
+package remove_element
+
+import (
+	"testing"
+)
+
+type caseType struct {
+	input    []int
+	val      int
+	expected []int
+}
+
+func TestRemoveElement(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{3, 2, 2, 3},
+			val:      3,
+			expected: []int{2, 2},
+		},
+		{
+			input:    []int{0, 1, 2, 2, 3, 0, 4, 2},
+			val:      2,
+			expected: []int{0, 1, 3, 0, 4},
+		},
+		{
+			input:    []int{1, 2, 3, 4, 5},
+			val:      6,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			input:    []int{1, 2, 2, 3, 3, 3, 4, 4, 4, 4},
+			val:      4,
+			expected: []int{1, 2, 2, 3, 3, 3},
+		},
+		{
+			input:    nil,
+			val:      1,
+			expected: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		nums := make([]int, len(tc.input))
+		copy(nums, tc.input)
+		l := removeElement(nums, tc.val)
+		output := nums[:l]
+		if l != len(tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+		for k, v := range tc.expected {
+			if output[k] != v {
+				t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+			}
+		}
+	}
+}
