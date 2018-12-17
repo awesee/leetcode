57dec751bbffed937fcf93edd62f42e2600612e0
Openset <openset.wang@gmail.com>
2018-12-17 17:54:58 +0800

    Add: implement_strstr

---
+++ solution/implement-strstr/implement_strstr_test.go
@@ -0,0 +1,30 @@
+package implement_strstr
+
+import "testing"
+
+type caseType struct {
+	haystack string
+	needle   string
+	expected int
+}
+
+func TestTwoSum(t *testing.T) {
+	tests := [...]caseType{
+		{
+			haystack: "hello",
+			needle:   "ll",
+			expected: 2,
+		}, {
+			haystack: "this is test string",
+			needle:   "a",
+			expected: -1,
+		},
+	}
+
+	for _, tc := range tests {
+		output := strStr(tc.haystack, tc.needle)
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.haystack, tc.needle, output, tc.expected)
+		}
+	}
+}
