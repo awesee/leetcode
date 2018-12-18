547e1a51d62810890e43d1b4a072014c37f909e8
Openset <openset.wang@gmail.com>
2018-12-18 22:54:15 +0800

    Add: count_and_say

--- solution/count-and-say/count_and_say.go
+++ solution/count-and-say/count_and_say.go
@@ -1,2 +1,33 @@
 package count_and_say
 
+func countAndSay(n int) string {
+	r := "1"
+	for i := 1; i < n; i++ {
+		r = say(r)
+	}
+	return r
+}
+
+func say(s string) string {
+	r := ""
+	var sv rune
+	var sc byte
+	for i, v := range s {
+		if i == 0 {
+			sv = v
+			sc = '1'
+		} else {
+			if v == rune(s[i-1]) {
+				sc++
+			} else {
+				r += string(sc) + string(sv)
+				sv = v
+				sc = '1'
+			}
+		}
+	}
+	if sc > '0' {
+		r += string(sc) + string(sv)
+	}
+	return r
+}
