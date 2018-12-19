e3ecf432d63221f1b280105e1328e34807b87bdc
Openset <openset.wang@gmail.com>
2018-12-19 14:16:24 +0800

    Add: saveCredential

--- internal/leetcode/login.go
+++ internal/leetcode/login.go
@@ -6,15 +6,16 @@ import (
 	"strings"
 )
 
-func AccountsLogin(user string, pwd string) (*http.Response, error) {
+func AccountsLogin(username, password string) (*http.Response, error) {
 	resp, err := http.Head(AccountsLoginUrl)
 	checkErr(err)
+	defer resp.Body.Close()
 	cookies := resp.Cookies()
 	saveCookies(cookies)
 	csrftoken := getCsrfToken(cookies)
 	data := url.Values{
-		"login":               {user},
-		"password":            {pwd},
+		"login":               {username},
+		"password":            {password},
 		"csrfmiddlewaretoken": {csrftoken},
 	}
 	req, err := http.NewRequest("POST", AccountsLoginUrl, strings.NewReader(data.Encode()))
@@ -26,6 +27,10 @@ func AccountsLogin(user string, pwd string) (*http.Response, error) {
 	}
 	resp, err = http.DefaultClient.Do(req)
 	checkErr(err)
+	defer resp.Body.Close()
 	saveCookies(resp.Cookies())
+	if resp.StatusCode == 200 {
+		saveCredential(data)
+	}
 	return resp, err
 }
