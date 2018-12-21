952740c8d941420eb21cee5f0dfdce74c63d2f53
Openset <openset.wang@gmail.com>
2018-12-21 11:39:10 +0800

    Add: fizz_buzz

--- solution/fizz-buzz/fizz_buzz.go
+++ solution/fizz-buzz/fizz_buzz.go
@@ -1,2 +1,21 @@
 package fizz_buzz
 
+import "strconv"
+
+func fizzBuzz(n int) []string {
+	s := make([]string, 0, n)
+	for i := 1; i <= n; i++ {
+		str := ""
+		if i%3 == 0 {
+			str += "Fizz"
+		}
+		if i%5 == 0 {
+			str += "Buzz"
+		}
+		if str == "" {
+			str = strconv.Itoa(i)
+		}
+		s = append(s, str)
+	}
+	return s
+}
--- solution/fizz-buzz/fizz_buzz_test.go
+++ solution/fizz-buzz/fizz_buzz_test.go
@@ -1,2 +1,70 @@
 package fizz_buzz
 
+import "testing"
+
+type caseType struct {
+	input    int
+	expected []string
+}
+
+func TestFizzBuzz(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input: 15,
+			expected: []string{
+				"1",
+				"2",
+				"Fizz",
+				"4",
+				"Buzz",
+				"Fizz",
+				"7",
+				"8",
+				"Fizz",
+				"Buzz",
+				"11",
+				"Fizz",
+				"13",
+				"14",
+				"FizzBuzz",
+			},
+		},
+		{
+			input: 5,
+			expected: []string{
+				"1",
+				"2",
+				"Fizz",
+				"4",
+				"Buzz",
+			},
+		},
+		{
+			input: 10,
+			expected: []string{
+				"1",
+				"2",
+				"Fizz",
+				"4",
+				"Buzz",
+				"Fizz",
+				"7",
+				"8",
+				"Fizz",
+				"Buzz",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		output := fizzBuzz(tc.input)
+		if len(output) != len(tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+		for k, v := range tc.expected {
+			if output[k] != v {
+				t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+			}
+		}
+	}
+}
