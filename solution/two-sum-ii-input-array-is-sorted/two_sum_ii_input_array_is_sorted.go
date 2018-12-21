bea1b6a821d4c8f785c2ab30e01d5d06b0026545
Openset <openset.wang@gmail.com>
2018-12-21 13:52:58 +0800

    Add: two_sum_ii_input_array_is_sorted

--- solution/two-sum-ii-input-array-is-sorted/two_sum_ii_input_array_is_sorted.go
+++ solution/two-sum-ii-input-array-is-sorted/two_sum_ii_input_array_is_sorted.go
@@ -1,2 +1,17 @@
 package two_sum_ii_input_array_is_sorted
 
+func twoSum(numbers []int, target int) []int {
+	s := 0
+	l := len(numbers) - 1
+	for s < l {
+		r := numbers[s] + numbers[l]
+		if r == target {
+			return []int{s + 1, l + 1}
+		} else if r < target {
+			s++
+		} else {
+			l--
+		}
+	}
+	return nil
+}
