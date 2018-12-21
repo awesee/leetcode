1d3218c8b017ab01f3018f01e3dae276aebed4b2
Openset <openset.wang@gmail.com>
2018-12-21 14:27:40 +0800

    Add: first_unique_character_in_a_string

--- solution/first-unique-character-in-a-string/first_unique_character_in_a_string.go
+++ solution/first-unique-character-in-a-string/first_unique_character_in_a_string.go
@@ -1,2 +1,17 @@
 package first_unique_character_in_a_string
 
+func firstUniqChar(s string) int {
+	var count [256]byte
+	l := len(s)
+	for i := 0; i < l; i++ {
+		if count[s[i]] < 2 {
+			count[s[i]]++
+		}
+	}
+	for i := 0; i < l; i++ {
+		if count[s[i]] == 1 {
+			return i
+		}
+	}
+	return -1
+}
--- solution/first-unique-character-in-a-string/first_unique_character_in_a_string_test.go
+++ solution/first-unique-character-in-a-string/first_unique_character_in_a_string_test.go
@@ -1,2 +1,18 @@
 package first_unique_character_in_a_string
 
+import "testing"
+
+func TestFirstUniqChar(t *testing.T) {
+	tests := map[string]int{
+		"leetcode":     0,
+		"loveleetcode": 2,
+		"hello world":  0,
+	}
+
+	for input, expected := range tests {
+		output := firstUniqChar(input)
+		if output != expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", input, output, expected)
+		}
+	}
+}
