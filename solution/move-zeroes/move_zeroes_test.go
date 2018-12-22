c69d7c93128a7db7571b264b9457b4b4caca4324
Openset <openset.wang@gmail.com>
2018-12-22 16:04:30 +0800

    Add: move_zeroes

--- solution/move-zeroes/move_zeroes_test.go
+++ solution/move-zeroes/move_zeroes_test.go
@@ -1,2 +1,37 @@
 package move_zeroes
 
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    []int
+	expected []int
+}
+
+func TestMoveZeroes(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{0, 1, 0, 3, 12},
+			expected: []int{1, 3, 12, 0, 0},
+		},
+		{
+			input:    []int{0, 1, 0, 2, 3, 3, 0, 7, 8},
+			expected: []int{1, 2, 3, 3, 7, 8, 0, 0, 0},
+		},
+		{
+			input:    []int{1, 2, 3, 4, 5},
+			expected: []int{1, 2, 3, 4, 5},
+		},
+	}
+
+	for _, tc := range tests {
+		output := make([]int, len(tc.input))
+		copy(output, tc.input)
+		moveZeroes(output)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
