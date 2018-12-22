b6337cf84136b446a705d60a84625b994a6c055d
Openset <openset.wang@gmail.com>
2018-12-22 20:28:51 +0800

    Add: count_primes

--- solution/count-primes/count_primes.go
+++ solution/count-primes/count_primes.go
@@ -1,2 +1,18 @@
 package count_primes
 
+func countPrimes(n int) int {
+	if n < 3 {
+		return 0
+	}
+	primes := make([]bool, n)
+	count := 1
+	for i := 3; i < n; i += 2 {
+		if !primes[i] {
+			count++
+			for j := i * i; j < n; j += i + i {
+				primes[j] = true
+			}
+		}
+	}
+	return count
+}
