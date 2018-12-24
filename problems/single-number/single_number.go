e3bef639067ae89f866ae1576651be17cc34b27e
Openset <openset.wang@gmail.com>
2018-12-24 16:27:09 +0800

    Add: single_number

--- problems/single-number/single_number.go
+++ problems/single-number/single_number.go
@@ -1,1 +1,9 @@
 package single_number
+
+func singleNumber(nums []int) int {
+	n := 0
+	for _, v := range nums {
+		n ^= v
+	}
+	return n
+}
--- problems/single-number/single_number_test.go
+++ problems/single-number/single_number_test.go
@@ -1,1 +1,32 @@
 package single_number
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestSingleNumber(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{2, 2, 1},
+			expected: 1,
+		},
+		{
+			input:    []int{4, 1, 2, 1, 2},
+			expected: 4,
+		},
+		{
+			input:    []int{1, 2, 1, 2, 3},
+			expected: 3,
+		},
+	}
+
+	for _, tc := range tests {
+		output := singleNumber(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
