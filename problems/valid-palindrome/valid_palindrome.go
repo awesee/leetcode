f33707d8b4126a4337e086cf9c76d0ec3e82abfd
Openset <openset.wang@gmail.com>
2018-12-24 17:27:22 +0800

    Add: valid_palindrome

--- problems/valid-palindrome/valid_palindrome.go
+++ problems/valid-palindrome/valid_palindrome.go
@@ -1,1 +1,33 @@
 package valid_palindrome
+
+func isPalindrome(s string) bool {
+	start, end := 0, len(s)-1
+	for start < end {
+		if !isAlphanumeric(s[start]) {
+			start++
+			continue
+		}
+		if !isAlphanumeric(s[end]) {
+			end--
+			continue
+		}
+		x := s[start]
+		if s[start] >= 'a' {
+			x = s[start] + 'A' - 'a'
+		}
+		y := s[end]
+		if s[end] >= 'a' {
+			y = s[end] + 'A' - 'a'
+		}
+		if x != y {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
+
+func isAlphanumeric(b byte) bool {
+	return ('0' <= b && b <= '9') || ('A' <= b && b <= 'Z') || ('a' <= b && b <= 'z')
+}
