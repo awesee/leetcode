9de5038ed26dd7991a53fe4a8cc04791bc3e402f
Openset <openset.wang@gmail.com>
2018-12-25 15:06:52 +0800

    Add: add_two_numbers

--- problems/add-two-numbers/add_two_numbers.go
+++ problems/add-two-numbers/add_two_numbers.go
@@ -1,1 +1,31 @@
 package add_two_numbers
+
+import . "github.com/openset/leetcode/problems/000000"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	r := &ListNode{}
+	t := r
+	carry := 0
+	for l1 != nil || l2 != nil || carry != 0 {
+		val := carry
+		if l1 != nil {
+			val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			val += l2.Val
+			l2 = l2.Next
+		}
+		t.Next = &ListNode{Val: val % 10}
+		t = t.Next
+		carry = val / 10
+	}
+	return r.Next
+}
--- problems/add-two-numbers/add_two_numbers_test.go
+++ problems/add-two-numbers/add_two_numbers_test.go
@@ -1,1 +1,43 @@
 package add_two_numbers
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/openset/leetcode/problems/000000"
+)
+
+type caseType struct {
+	l1       []int
+	l2       []int
+	expected []int
+}
+
+func TestAddTwoNumbers(t *testing.T) {
+	tests := [...]caseType{
+		{
+			l1:       []int{2, 4, 3},
+			l2:       []int{5, 6, 4},
+			expected: []int{7, 0, 8},
+		},
+		{
+			l1:       []int{5},
+			l2:       []int{5},
+			expected: []int{0, 1},
+		},
+		{
+			l1:       []int{5},
+			l2:       []int{5, 9, 9},
+			expected: []int{0, 0, 0, 1},
+		},
+	}
+
+	for _, tc := range tests {
+		l1 := base.SliceInt2ListNode(tc.l1)
+		l2 := base.SliceInt2ListNode(tc.l2)
+		output := base.ListNode2SliceInt(addTwoNumbers(l1, l2))
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.l1, tc.l2, output, tc.expected)
+		}
+	}
+}
