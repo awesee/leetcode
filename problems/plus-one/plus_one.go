c88c1fce346352b3749e8473ef797e043d1ff441
Openset <openset.wang@gmail.com>
2018-12-25 10:01:31 +0800

    Add: plus_one

--- problems/plus-one/plus_one.go
+++ problems/plus-one/plus_one.go
@@ -1,1 +1,16 @@
 package plus_one
+
+func plusOne(digits []int) []int {
+	l := len(digits)
+	for i := l - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	if digits[0] == 0 {
+		digits = append([]int{1}, digits...)
+	}
+	return digits
+}
