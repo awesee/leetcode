c979212903a082bcb88671537636432d753bf2d6
Openset <openset.wang@gmail.com>
2018-12-25 14:00:31 +0800

    Add: length_of_last_word

--- problems/length-of-last-word/length_of_last_word_test.go
+++ problems/length-of-last-word/length_of_last_word_test.go
@@ -1,1 +1,22 @@
 package length_of_last_word
+
+import "testing"
+
+func TestLengthOfLastWord(t *testing.T) {
+	tests := map[string]int{
+		"Hello World":      5,
+		"Hello":            5,
+		"this is a string": 6,
+		"":                 0,
+		" ":                0,
+		"a ":               1,
+		"b  ":              1,
+	}
+
+	for input, expected := range tests {
+		output := lengthOfLastWord(input)
+		if output != expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", input, output, expected)
+		}
+	}
+}
