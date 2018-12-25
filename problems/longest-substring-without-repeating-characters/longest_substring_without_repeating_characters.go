c93fd3475ef74dd59caafb1e57ce0f9d604c6f49
Openset <openset.wang@gmail.com>
2018-12-25 16:20:19 +0800

    Add: longest_substring_without_repeating_characters

--- problems/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.go
+++ problems/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.go
@@ -1,1 +1,16 @@
 package longest_substring_without_repeating_characters
+
+func lengthOfLongestSubstring(s string) int {
+	ans, l := 0, len(s)
+	index := [128]int{}
+	for i, j := 0, 0; j < l; j++ {
+		if i < index[s[j]] {
+			i = index[s[j]]
+		}
+		if ans < j-i+1 {
+			ans = j - i + 1
+		}
+		index[s[j]] = j + 1
+	}
+	return ans
+}
--- problems/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters_test.go
+++ problems/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters_test.go
@@ -1,1 +1,19 @@
 package longest_substring_without_repeating_characters
+
+import "testing"
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	tests := map[string]int{
+		"abcabcbb": 3,
+		"bbbbb":    1,
+		"pwwkew":   3,
+		"abcdef":   6,
+	}
+
+	for input, expected := range tests {
+		output := lengthOfLongestSubstring(input)
+		if output != expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", input, output, expected)
+		}
+	}
+}
