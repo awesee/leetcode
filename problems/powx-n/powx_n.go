00412c8d798434e33c61cb9334acb5cedf0a90d0
Openset <openset.wang@gmail.com>
2018-12-25 10:36:39 +0800

    Add: powx_n

--- problems/powx-n/powx_n.go
+++ problems/powx-n/powx_n.go
@@ -1,1 +1,16 @@
 package powx_n
+
+func myPow(x float64, n int) float64 {
+	res := 1.0
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+	for ; n > 0; n >>= 1 {
+		if n&1 == 1 {
+			res *= x
+		}
+		x *= x
+	}
+	return res
+}
--- problems/powx-n/powx_n_test.go
+++ problems/powx-n/powx_n_test.go
@@ -1,1 +1,40 @@
 package powx_n
+
+import (
+	"math"
+	"testing"
+)
+
+type caseType struct {
+	x        float64
+	n        int
+	expected float64
+}
+
+func TestMyPow(t *testing.T) {
+	tests := [...]caseType{
+		{
+			x:        2.00000,
+			n:        10,
+			expected: 1024.00000,
+		},
+		{
+			x:        2.10000,
+			n:        3,
+			expected: 9.26100,
+		},
+		{
+			x:        2.00000,
+			n:        -2,
+			expected: 0.25000,
+		},
+	}
+
+	for _, tc := range tests {
+		output := myPow(tc.x, tc.n)
+		output = math.Trunc(output*1e5) / 1e5
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.x, tc.n, output, tc.expected)
+		}
+	}
+}
