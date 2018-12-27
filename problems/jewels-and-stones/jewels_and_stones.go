aec21198c9d988c1413881739bc563a0bbabe148
Openset <openset.wang@gmail.com>
2018-12-27 10:09:07 +0800

    Add: jewels_and_stones

--- problems/jewels-and-stones/jewels_and_stones.go
+++ problems/jewels-and-stones/jewels_and_stones.go
@@ -1,1 +1,15 @@
 package jewels_and_stones
+
+func numJewelsInStones(J string, S string) int {
+	m := [128]bool{}
+	for i := 0; i < len(J); i++ {
+		m[J[i]] = true
+	}
+	count := 0
+	for i := 0; i < len(S); i++ {
+		if m[S[i]] {
+			count++
+		}
+	}
+	return count
+}
