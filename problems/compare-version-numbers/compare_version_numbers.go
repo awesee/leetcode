5b9303dbca6bdb6acecf30857f30e975a192da1f
Openset <openset.wang@gmail.com>
2018-12-28 16:24:01 +0800

    Add: compare_version_numbers

--- problems/compare-version-numbers/compare_version_numbers.go
+++ problems/compare-version-numbers/compare_version_numbers.go
@@ -1,1 +1,34 @@
 package compare_version_numbers
+
+import (
+	"strconv"
+	"strings"
+)
+
+func compareVersion(version1 string, version2 string) int {
+	v1s := strings.Split(version1, ".")
+	v2s := strings.Split(version2, ".")
+	i, l1 := 0, len(v1s)
+	j, l2 := 0, len(v2s)
+	for i < l1 || j < l2 {
+		v1, v2 := "0", "0"
+		if i < l1 {
+			v1 = v1s[i]
+			i++
+		}
+		if j < l2 {
+			v2 = v2s[j]
+			j++
+		}
+		n1, _ := strconv.Atoi(v1)
+		n2, _ := strconv.Atoi(v2)
+		if n1 != n2 {
+			if n1 < n2 {
+				return -1
+			} else {
+				return 1
+			}
+		}
+	}
+	return 0
+}
--- problems/compare-version-numbers/compare_version_numbers_test.go
+++ problems/compare-version-numbers/compare_version_numbers_test.go
@@ -1,1 +1,66 @@
 package compare_version_numbers
+
+import "testing"
+
+type caseType struct {
+	v1       string
+	v2       string
+	expected int
+}
+
+func TestCompareVersion(t *testing.T) {
+	tests := [...]caseType{
+		{
+			v1:       "1.0.01",
+			v2:       "1.00.1",
+			expected: 0,
+		},
+		{
+			v1:       "1.0.0",
+			v2:       "1",
+			expected: 0,
+		},
+		{
+			v1:       "0.1",
+			v2:       "1.1",
+			expected: -1,
+		},
+		{
+			v1:       "7.5.2.4",
+			v2:       "7.5.3",
+			expected: -1,
+		},
+
+		{
+			v1:       "7.00",
+			v2:       "7.001",
+			expected: -1,
+		},
+		{
+			v1:       "1.1",
+			v2:       "1.10",
+			expected: -1,
+		},
+		{
+			v1:       "1.0.1",
+			v2:       "1",
+			expected: 1,
+		},
+		{
+			v1:       "1.10",
+			v2:       "1.2",
+			expected: 1,
+		},
+		{
+			v1:       "1.10.05",
+			v2:       "1.9.10",
+			expected: 1,
+		},
+	}
+	for _, tc := range tests {
+		output := compareVersion(tc.v1, tc.v2)
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.v1, tc.v2, output, tc.expected)
+		}
+	}
+}
