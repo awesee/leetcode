afab65816f8d79213af77b0c64ecc1edeac3cc34
Openset <openset.wang@gmail.com>
2018-12-28 10:24:39 +0800

    Add: unique_email_addresses

--- problems/unique-email-addresses/unique_email_addresses.go
+++ problems/unique-email-addresses/unique_email_addresses.go
@@ -1,1 +1,21 @@
 package unique_email_addresses
+
+import "strings"
+
+func numUniqueEmails(emails []string) int {
+	m := make(map[string]bool)
+	count := 0
+	for _, email := range emails {
+		plusIndex := strings.Index(email, "+")
+		atIndex := strings.Index(email, "@")
+		if plusIndex == -1 {
+			plusIndex = atIndex
+		}
+		email = strings.Replace(email[0:plusIndex], ".", "", -1) + email[atIndex:]
+		if !m[email] {
+			m[email] = true
+			count++
+		}
+	}
+	return count
+}
