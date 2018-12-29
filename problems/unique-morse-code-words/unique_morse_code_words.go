b6579cb1e43723d1d9ec856f18d7bd9451663275
Openset <openset.wang@gmail.com>
2018-12-29 09:58:22 +0800

    Add: unique_morse_code_words

--- problems/unique-morse-code-words/unique_morse_code_words.go
+++ problems/unique-morse-code-words/unique_morse_code_words.go
@@ -1,1 +1,14 @@
 package unique_morse_code_words
+
+func uniqueMorseRepresentations(words []string) int {
+	list := [...]string{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."}
+	m := make(map[string]bool)
+	for _, word := range words {
+		k := ""
+		for i := 0; i < len(word); i++ {
+			k += list[word[i]-'a']
+		}
+		m[k] = true
+	}
+	return len(m)
+}
--- problems/unique-morse-code-words/unique_morse_code_words_test.go
+++ problems/unique-morse-code-words/unique_morse_code_words_test.go
@@ -1,1 +1,27 @@
 package unique_morse_code_words
+
+import "testing"
+
+type caseType struct {
+	input    []string
+	expected int
+}
+
+func TestUniqueMorseRepresentations(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []string{"gin", "zen", "gig", "msg"},
+			expected: 2,
+		},
+		{
+			input:    []string{"hello", "word"},
+			expected: 2,
+		},
+	}
+	for _, tc := range tests {
+		output := uniqueMorseRepresentations(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
