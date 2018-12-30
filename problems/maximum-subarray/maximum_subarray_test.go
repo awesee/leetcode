350d144b2c4dd1511e88fa5d6dc15183d564811e
Openset <openset.wang@gmail.com>
2018-12-30 15:19:23 +0800

    Add: maximum_subarray

--- problems/maximum-subarray/maximum_subarray_test.go
+++ problems/maximum-subarray/maximum_subarray_test.go
@@ -1,1 +1,23 @@
 package maximum_subarray
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestMaxSubArray(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{-2, 1, -3, 4, -1, 2, 1, -5, 4},
+			expected: 6,
+		},
+	}
+	for _, tc := range tests {
+		output := maxSubArray(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
