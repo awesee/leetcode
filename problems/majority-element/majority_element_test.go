ab175ba4ce414f6a7c0ef779d8c365c8b52223b6
Openset <openset.wang@gmail.com>
2018-12-30 15:46:54 +0800

    Add: majority_element

--- problems/majority-element/majority_element_test.go
+++ problems/majority-element/majority_element_test.go
@@ -1,1 +1,31 @@
 package majority_element
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestMajorityElement(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{3, 2, 3},
+			expected: 3,
+		},
+		{
+			input:    []int{2, 2, 1, 1, 1, 2, 2},
+			expected: 2,
+		},
+		{
+			input:    []int{1},
+			expected: 1,
+		},
+	}
+	for _, tc := range tests {
+		output := majorityElement(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
