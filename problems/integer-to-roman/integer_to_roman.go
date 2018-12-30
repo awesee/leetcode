c574f3a505d9515e8988b78ddd2f410f0bbad190
Openset <openset.wang@gmail.com>
2018-12-30 14:04:07 +0800

    Add: integer_to_roman

--- problems/integer-to-roman/integer_to_roman.go
+++ problems/integer-to-roman/integer_to_roman.go
@@ -1,1 +1,14 @@
 package integer_to_roman
+
+func intToRoman(num int) string {
+	base := [...]int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	bs := [...]string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	ans := ""
+	for i, b := range base {
+		for num/b > 0 {
+			ans += bs[i]
+			num -= b
+		}
+	}
+	return ans
+}
