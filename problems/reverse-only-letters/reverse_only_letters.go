a32c85a8dbdcb1c21084e8277cb403bd181c73bf
Openset <openset.wang@gmail.com>
2018-12-31 10:08:23 +0800

    Add: reverse_only_letters

--- problems/reverse-only-letters/reverse_only_letters.go
+++ problems/reverse-only-letters/reverse_only_letters.go
@@ -1,1 +1,20 @@
 package reverse_only_letters
+
+import "unicode"
+
+func reverseOnlyLetters(S string) string {
+	s := []rune(S)
+	i, j := 0, len(s)-1
+	for i < j {
+		if !unicode.IsLetter(rune(S[i])) {
+			i++
+		} else if !unicode.IsLetter(rune(S[j])) {
+			j--
+		} else {
+			s[i], s[j] = s[j], s[i]
+			i++
+			j--
+		}
+	}
+	return string(s)
+}
--- problems/reverse-only-letters/reverse_only_letters_test.go
+++ problems/reverse-only-letters/reverse_only_letters_test.go
@@ -1,1 +1,31 @@
 package reverse_only_letters
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected string
+}
+
+func TestReverseOnlyLetters(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "ab-cd",
+			expected: "dc-ba",
+		},
+		{
+			input:    "a-bC-dEf-ghIj",
+			expected: "j-Ih-gfE-dCba",
+		},
+		{
+			input:    "Test1ng-Leet=code-Q!",
+			expected: "Qedo1ct-eeLg=ntse-T!",
+		},
+	}
+	for _, tc := range tests {
+		output := reverseOnlyLetters(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
