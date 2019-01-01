564dee5b76b746febe19dfb43e9fa698e906e9a0
Openset <openset.wang@gmail.com>
2019-01-01 11:21:00 +0800

    Add: buddy_strings

--- problems/buddy-strings/buddy_strings.go
+++ problems/buddy-strings/buddy_strings.go
@@ -1,1 +1,32 @@
 package buddy_strings
+
+func buddyStrings(A string, B string) bool {
+	if len(A) != len(B) {
+		return false
+	}
+	if A == B {
+		exist := [26]bool{}
+		for _, c := range A {
+			k := c - 'a'
+			if exist[k] {
+				return true
+			}
+			exist[k] = true
+		}
+		return false
+	} else {
+		m, n := -1, -1
+		for i, c := range A {
+			if B[i] != byte(c) {
+				if m == -1 {
+					m = i
+				} else if n == -1 {
+					n = i
+				} else {
+					return false
+				}
+			}
+		}
+		return n != -1 && A[m] == B[n] && A[n] == B[m]
+	}
+}
--- problems/buddy-strings/buddy_strings_test.go
+++ problems/buddy-strings/buddy_strings_test.go
@@ -1,1 +1,40 @@
 package buddy_strings
+
+import "testing"
+
+type caseType struct {
+	a        string
+	b        string
+	expected bool
+}
+
+func TestBuddyStrings(t *testing.T) {
+	tests := [...]caseType{
+		{
+			a:        "ab",
+			b:        "ba",
+			expected: true,
+		},
+		{
+			a:        "ab",
+			b:        "ab",
+			expected: false,
+		},
+		{
+			a:        "aaaaaaabc",
+			b:        "aaaaaaacb",
+			expected: true,
+		},
+		{
+			a:        "",
+			b:        "aa",
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := buddyStrings(tc.a, tc.b)
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.a, tc.b, output, tc.expected)
+		}
+	}
+}
