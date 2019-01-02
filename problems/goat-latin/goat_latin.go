598263538259e90e521e73b32794a223739212b3
Openset <openset.wang@gmail.com>
2019-01-02 17:09:28 +0800

    Add: goat_latin

--- problems/goat-latin/goat_latin.go
+++ problems/goat-latin/goat_latin.go
@@ -1,1 +1,18 @@
 package goat_latin
+
+import "strings"
+
+func toGoatLatin(S string) string {
+	ans := ""
+	s := strings.Split(S, " ")
+	for i, word := range s {
+		switch word[0] {
+		case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+			ans += " " + word + "ma"
+		default:
+			ans += " " + word[1:] + word[0:1] + "ma"
+		}
+		ans += strings.Repeat("a", i+1)
+	}
+	return ans[1:]
+}
--- problems/goat-latin/goat_latin_test.go
+++ problems/goat-latin/goat_latin_test.go
@@ -1,1 +1,27 @@
 package goat_latin
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected string
+}
+
+func TestToGoatLatin(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "I speak Goat Latin",
+			expected: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa",
+		},
+		{
+			input:    "The quick brown fox jumped over the lazy dog",
+			expected: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa",
+		},
+	}
+	for _, tc := range tests {
+		output := toGoatLatin(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
