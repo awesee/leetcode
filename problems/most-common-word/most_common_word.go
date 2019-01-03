028ffc1207e2297a1976832ca338c3868c26c1e5
Openset <openset.wang@gmail.com>
2019-01-03 11:00:18 +0800

    Add: most_common_word

--- problems/most-common-word/most_common_word.go
+++ problems/most-common-word/most_common_word.go
@@ -1,1 +1,31 @@
 package most_common_word
+
+import "strings"
+
+func mostCommonWord(paragraph string, banned []string) string {
+	word, words := "", make([]string, 0)
+	paragraph = strings.ToLower(paragraph) + " "
+	for _, char := range paragraph {
+		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') {
+			word += string(char)
+		} else if word != "" {
+			words = append(words, word)
+			word = ""
+		}
+	}
+	m := make(map[string]bool)
+	for _, ban := range banned {
+		m[ban] = true
+	}
+	ans, max, count := "", 0, make(map[string]int)
+	for _, word = range words {
+		if !m[word] {
+			count[word]++
+			if count[word] > max {
+				max = count[word]
+				ans = word
+			}
+		}
+	}
+	return ans
+}
--- problems/most-common-word/most_common_word_test.go
+++ problems/most-common-word/most_common_word_test.go
@@ -1,1 +1,35 @@
 package most_common_word
+
+import "testing"
+
+type caseType struct {
+	paragraph string
+	banned    []string
+	expected  string
+}
+
+func TestMostCommonWord(t *testing.T) {
+	tests := [...]caseType{
+		{
+			paragraph: "Bob hit a ball, the hit BALL flew far after it was hit.",
+			banned:    []string{"hit"},
+			expected:  "ball",
+		},
+		{
+			paragraph: "a, a, a, a, b,b,b,c, c",
+			banned:    []string{"a"},
+			expected:  "b",
+		},
+		{
+			paragraph: "Bob",
+			banned:    []string{},
+			expected:  "bob",
+		},
+	}
+	for _, tc := range tests {
+		output := mostCommonWord(tc.paragraph, tc.banned)
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.paragraph, tc.banned, output, tc.expected)
+		}
+	}
+}
