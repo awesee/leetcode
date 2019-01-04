d3bdeae36716891b8400b7faceafdae39f6f7919
Openset <openset.wang@gmail.com>
2019-01-04 18:30:18 +0800

    Add: single_number_ii

--- problems/single-number-ii/single_number_ii.go
+++ problems/single-number-ii/single_number_ii.go
@@ -1,1 +1,10 @@
 package single_number_ii
+
+func singleNumber(nums []int) int {
+	ones, twos := 0, 0
+	for _, n := range nums {
+		ones = (ones ^ n) & ^twos
+		twos = (twos ^ n) & ^ones
+	}
+	return ones
+}
