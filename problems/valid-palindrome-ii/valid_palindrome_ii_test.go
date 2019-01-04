ba506a7cae41a3a858b655bf9117a7b7c7c76133
Openset <openset.wang@gmail.com>
2019-01-04 17:35:20 +0800

    Add: valid_palindrome_ii

--- problems/valid-palindrome-ii/valid_palindrome_ii_test.go
+++ problems/valid-palindrome-ii/valid_palindrome_ii_test.go
@@ -1,1 +1,39 @@
 package valid_palindrome_ii
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected bool
+}
+
+func TestValidPalindrome(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "aba",
+			expected: true,
+		},
+		{
+			input:    "abca",
+			expected: true,
+		},
+		{
+			input:    "hello",
+			expected: false,
+		},
+		{
+			input:    "abcdcbda",
+			expected: true,
+		},
+		{
+			input:    "abcbabbbca",
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := validPalindrome(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
