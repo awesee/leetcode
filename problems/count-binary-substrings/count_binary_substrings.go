18d6ea04dce4676a74261ebd04a439f8198a46a8
Openset <openset.wang@gmail.com>
2019-01-04 14:03:38 +0800

    Add: count_binary_substrings

--- problems/count-binary-substrings/count_binary_substrings.go
+++ problems/count-binary-substrings/count_binary_substrings.go
@@ -1,1 +1,22 @@
 package count_binary_substrings
+
+func countBinarySubstrings(s string) int {
+	ans, pre, cur, l := 0, 0, 1, len(s)
+	for i := 1; i < l; i++ {
+		if s[i] != s[i-1] {
+			ans += min(pre, cur)
+			pre = cur
+			cur = 1
+		} else {
+			cur++
+		}
+	}
+	return ans + min(pre, cur)
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
