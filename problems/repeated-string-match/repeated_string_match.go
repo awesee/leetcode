b4d357fc4039622f5e85dfd3f8bce9061aff2fb5
Openset <openset.wang@gmail.com>
2019-01-05 10:01:42 +0800

    Add: repeated_string_match

--- problems/repeated-string-match/repeated_string_match.go
+++ problems/repeated-string-match/repeated_string_match.go
@@ -1,1 +1,18 @@
 package repeated_string_match
+
+import "strings"
+
+func repeatedStringMatch(A string, B string) int {
+	a, count := A, len(B)/len(A)
+	if len(B)%len(A) > 0 {
+		count++
+	}
+	for times := 2; times > 0; times-- {
+		a = strings.Repeat(A, count)
+		if strings.Index(a, B) != -1 {
+			return count
+		}
+		count++
+	}
+	return -1
+}
