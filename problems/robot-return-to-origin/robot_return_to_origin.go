8cfc62348bd5f720882d7cd2c7dee1b4b71e93ae
Openset <openset.wang@gmail.com>
2019-01-05 10:28:47 +0800

    Add: robot_return_to_origin

--- problems/robot-return-to-origin/robot_return_to_origin.go
+++ problems/robot-return-to-origin/robot_return_to_origin.go
@@ -1,1 +1,18 @@
 package robot_return_to_origin
+
+func judgeCircle(moves string) bool {
+	x, y := 0, 0
+	for _, move := range moves {
+		switch move {
+		case 'R':
+			x++
+		case 'L':
+			x--
+		case 'U':
+			y++
+		case 'D':
+			y--
+		}
+	}
+	return x == 0 && y == 0
+}
--- problems/robot-return-to-origin/robot_return_to_origin_test.go
+++ problems/robot-return-to-origin/robot_return_to_origin_test.go
@@ -1,1 +1,35 @@
 package robot_return_to_origin
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected bool
+}
+
+func TestJudgeCircle(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "UD",
+			expected: true,
+		},
+		{
+			input:    "RL",
+			expected: true,
+		},
+		{
+			input:    "LL",
+			expected: false,
+		},
+		{
+			input:    "UU",
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := judgeCircle(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
