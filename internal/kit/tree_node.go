072f63233251472cf0d351ea44d327c589eccf1f
Openset <openset.wang@gmail.com>
2019-01-07 21:57:40 +0800

    Add: tree_node

--- internal/kit/tree_node.go
+++ internal/kit/tree_node.go
@@ -18,11 +18,9 @@ func SliceInt2TreeNode(s []int) *TreeNode {
 		return nil
 	}
 	tree := &TreeNode{Val: s[0]}
-	queue := make([]*TreeNode, 1)
-	queue[0] = tree
+	queue := make([]*TreeNode, 0)
+	node := tree
 	for i := 1; i < l; i++ {
-		node := queue[0]
-		queue = queue[1:]
 		if s[i] != NULL {
 			node.Left = &TreeNode{Val: s[i]}
 			queue = append(queue, node.Left)
@@ -32,11 +30,35 @@ func SliceInt2TreeNode(s []int) *TreeNode {
 			node.Right = &TreeNode{Val: s[i]}
 			queue = append(queue, node.Right)
 		}
+		if len(queue) > 0 {
+			node = queue[0]
+			queue = queue[1:]
+		} else {
+			break
+		}
 	}
 	return tree
 }
 
 // convert *TreeNode to []int
 func TreeNode2SliceInt(t *TreeNode) (s []int) {
+	if t != nil {
+		queue := make([]*TreeNode, 1)
+		queue[0] = t
+		for len(queue) > 0 {
+			if queue[0] != nil {
+				s = append(s, queue[0].Val)
+				if queue[0].Left != nil || queue[0].Right != nil {
+					queue = append(queue, queue[0].Left)
+				}
+				if queue[0].Right != nil {
+					queue = append(queue, queue[0].Right)
+				}
+			} else {
+				s = append(s, NULL)
+			}
+			queue = queue[1:]
+		}
+	}
 	return
 }
