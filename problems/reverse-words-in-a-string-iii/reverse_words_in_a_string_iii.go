068728449a90eb776adca8269f56ad0df319501c
Openset <openset.wang@gmail.com>
2019-01-08 10:09:24 +0800

    Add: reverse_words_in_a_string_iii

--- problems/reverse-words-in-a-string-iii/reverse_words_in_a_string_iii.go
+++ problems/reverse-words-in-a-string-iii/reverse_words_in_a_string_iii.go
@@ -1,1 +1,15 @@
 package reverse_words_in_a_string_iii
+
+func reverseWords(s string) string {
+	ws, pre, l := []byte(s+" "), 0, len(s)
+	for cur, c := range ws {
+		if c == ' ' {
+			word := ws[pre:cur]
+			for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
+				word[i], word[j] = word[j], word[i]
+			}
+			pre = cur + 1
+		}
+	}
+	return string(ws[:l])
+}
--- problems/reverse-words-in-a-string-iii/reverse_words_in_a_string_iii_test.go
+++ problems/reverse-words-in-a-string-iii/reverse_words_in_a_string_iii_test.go
@@ -1,1 +1,23 @@
 package reverse_words_in_a_string_iii
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected string
+}
+
+func TestReverseWords(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "Let's take LeetCode contest",
+			expected: "s'teL ekat edoCteeL tsetnoc",
+		},
+	}
+	for _, tc := range tests {
+		output := reverseWords(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
