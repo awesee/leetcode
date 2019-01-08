70cbf2eb7a464946d7bbd5afc8d5273bd581f250
Openset <openset.wang@gmail.com>
2019-01-08 11:23:19 +0800

    Add: Student Attendance Record I (Easy)

--- problems/student-attendance-record-i/student_attendance_record_i_test.go
+++ problems/student-attendance-record-i/student_attendance_record_i_test.go
@@ -1,1 +1,27 @@
 package student_attendance_record_i
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected bool
+}
+
+func TestCheckRecord(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "PPALLP",
+			expected: true,
+		},
+		{
+			input:    "PPALLL",
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := checkRecord(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
