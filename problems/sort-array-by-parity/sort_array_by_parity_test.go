ba3bcf238219dfda00bef4caa69a31eef8117833
Openset <openset.wang@gmail.com>
2019-01-08 11:02:07 +0800

    Add: sort_array_by_parity

--- problems/sort-array-by-parity/sort_array_by_parity_test.go
+++ problems/sort-array-by-parity/sort_array_by_parity_test.go
@@ -1,1 +1,26 @@
 package sort_array_by_parity
+
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    []int
+	expected []int
+}
+
+func TestSortArrayByParity(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{3, 1, 2, 4},
+			expected: []int{2, 4, 3, 1},
+		},
+	}
+	for _, tc := range tests {
+		output := sortArrayByParity(tc.input)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
