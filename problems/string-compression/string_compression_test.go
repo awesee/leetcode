7673081ac6c25629803a522d97666d0ba08d301c
Openset <openset.wang@gmail.com>
2019-01-08 18:02:07 +0800

    Add: String Compression

--- problems/string-compression/string_compression_test.go
+++ problems/string-compression/string_compression_test.go
@@ -1,1 +1,38 @@
 package string_compression
+
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    []byte
+	expected []byte
+}
+
+func TestCompress(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []byte{'a', 'a', 'b', 'b', 'c', 'c', 'c'},
+			expected: []byte{'a', '2', 'b', '2', 'c', '3'},
+		},
+		{
+			input:    []byte{'a'},
+			expected: []byte{'a'},
+		},
+		{
+			input:    []byte{'a', 'a'},
+			expected: []byte{'a', '2'},
+		},
+		{
+			input:    []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+			expected: []byte{'a', 'b', '1', '2'},
+		},
+	}
+	for _, tc := range tests {
+		l := compress(tc.input)
+		if !reflect.DeepEqual(tc.input[:l], tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, l, tc.expected)
+		}
+	}
+}
