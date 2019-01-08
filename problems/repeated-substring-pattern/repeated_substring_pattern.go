3ddbad84b955e5f221e15b11b53ddf7fd9349cf9
Openset <openset.wang@gmail.com>
2019-01-08 16:41:29 +0800

    Add: Repeated Substring Pattern

--- problems/repeated-substring-pattern/repeated_substring_pattern.go
+++ problems/repeated-substring-pattern/repeated_substring_pattern.go
@@ -1,1 +1,13 @@
 package repeated_substring_pattern
+
+import "strings"
+
+func repeatedSubstringPattern(s string) bool {
+	l := len(s)
+	for i := 1; i <= l/2; i++ {
+		if l%i == 0 && strings.Count(s, s[:i]) == l/i {
+			return true
+		}
+	}
+	return false
+}
