dfd0838af020aba22b34f3d1566c2e4adb5eba2f
Openset <openset.wang@gmail.com>
2019-01-08 14:12:05 +0800

    Add: Longest Uncommon Subsequence I

--- problems/longest-uncommon-subsequence-i/longest_uncommon_subsequence_i_test.go
+++ problems/longest-uncommon-subsequence-i/longest_uncommon_subsequence_i_test.go
@@ -1,1 +1,40 @@
 package longest_uncommon_subsequence_i
+
+import "testing"
+
+type caseType struct {
+	a        string
+	b        string
+	expected int
+}
+
+func TestFindLUSlength(t *testing.T) {
+	tests := [...]caseType{
+		{
+			a:        "aba",
+			b:        "cdc",
+			expected: 3,
+		},
+		{
+			a:        "aba",
+			b:        "abcdef",
+			expected: 6,
+		},
+		{
+			a:        "abcdef",
+			b:        "cdc",
+			expected: 6,
+		},
+		{
+			a:        "abc",
+			b:        "abc",
+			expected: -1,
+		},
+	}
+	for _, tc := range tests {
+		output := findLUSlength(tc.a, tc.b)
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.a, tc.b, output, tc.expected)
+		}
+	}
+}
