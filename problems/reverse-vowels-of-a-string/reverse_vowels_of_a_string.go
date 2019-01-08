091baa753d3dd73688f095babfb3ccfb779ef94e
Openset <openset.wang@gmail.com>
2019-01-08 15:37:58 +0800

    Add: Reverse Vowels of a String

--- problems/reverse-vowels-of-a-string/reverse_vowels_of_a_string.go
+++ problems/reverse-vowels-of-a-string/reverse_vowels_of_a_string.go
@@ -1,1 +1,25 @@
 package reverse_vowels_of_a_string
+
+func reverseVowels(s string) string {
+	i, j, ss := 0, len(s)-1, []byte(s)
+	for i < j {
+		if !isVowel(ss[i]) {
+			i++
+		} else if !isVowel(ss[j]) {
+			j--
+		} else {
+			ss[i], ss[j] = ss[j], ss[i]
+			i++
+			j--
+		}
+	}
+	return string(ss)
+}
+
+func isVowel(r byte) bool {
+	switch r {
+	case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+		return true
+	}
+	return false
+}
--- problems/reverse-vowels-of-a-string/reverse_vowels_of_a_string_test.go
+++ problems/reverse-vowels-of-a-string/reverse_vowels_of_a_string_test.go
@@ -1,1 +1,27 @@
 package reverse_vowels_of_a_string
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected string
+}
+
+func TestReverseVowels(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "hello",
+			expected: "holle",
+		},
+		{
+			input:    "leetcode",
+			expected: "leotcede",
+		},
+	}
+	for _, tc := range tests {
+		output := reverseVowels(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
