031cea66b21c886375ca19a230ed9308bd167a14
Openset <openset.wang@gmail.com>
2019-01-08 14:58:03 +0800

    Add: Detect Capital

--- problems/detect-capital/detect_capital_test.go
+++ problems/detect-capital/detect_capital_test.go
@@ -1,1 +1,35 @@
 package detect_capital
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected bool
+}
+
+func TestDetectCapitalUse(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "USA",
+			expected: true,
+		},
+		{
+			input:    "leetcode",
+			expected: true,
+		},
+		{
+			input:    "Google",
+			expected: true,
+		},
+		{
+			input:    "FlaG",
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := detectCapitalUse(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
