2d929ee76f7942c264869ef73524e03555311148
Openset <openset.wang@gmail.com>
2019-01-08 16:54:37 +0800

    Update: Repeated Substring Pattern

--- problems/repeated-substring-pattern/repeated_substring_pattern.go
+++ problems/repeated-substring-pattern/repeated_substring_pattern.go
@@ -3,11 +3,5 @@ package repeated_substring_pattern
 import "strings"
 
 func repeatedSubstringPattern(s string) bool {
-	l := len(s)
-	for i := 1; i <= l/2; i++ {
-		if l%i == 0 && strings.Count(s, s[:i]) == l/i {
-			return true
-		}
-	}
-	return false
+	return strings.Contains((s + s)[1:2*len(s)-1], s)
 }
