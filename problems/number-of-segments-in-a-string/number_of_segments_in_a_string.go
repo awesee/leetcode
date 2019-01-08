336cae9c1bd8fd95d726ba6841c63e6282fe8b67
Openset <openset.wang@gmail.com>
2019-01-08 15:54:46 +0800

    Add: Number of Segments in a String

--- problems/number-of-segments-in-a-string/number_of_segments_in_a_string.go
+++ problems/number-of-segments-in-a-string/number_of_segments_in_a_string.go
@@ -1,1 +1,9 @@
 package number_of_segments_in_a_string
+
+import "regexp"
+
+func countSegments(s string) int {
+	reg := regexp.MustCompile(`\S+`)
+	matches := reg.FindAllString(s, -1)
+	return len(matches)
+}
--- problems/number-of-segments-in-a-string/number_of_segments_in_a_string_test.go
+++ problems/number-of-segments-in-a-string/number_of_segments_in_a_string_test.go
@@ -1,1 +1,39 @@
 package number_of_segments_in_a_string
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected int
+}
+
+func TestCountSegments(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "Hello, my name is John",
+			expected: 5,
+		},
+		{
+			input:    "",
+			expected: 0,
+		},
+		{
+			input:    " ",
+			expected: 0,
+		},
+		{
+			input:    " abc ",
+			expected: 1,
+		},
+		{
+			input:    "love live! mu'sic forever",
+			expected: 4,
+		},
+	}
+	for _, tc := range tests {
+		output := countSegments(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
