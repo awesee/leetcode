9e50dd5340970929864f3246adffc6a3e4d4711a
Openset <openset@users.noreply.github.com>
2019-01-10 14:04:32 +0800

    Add: Transpose Matrix

--- problems/transpose-matrix/transpose_matrix.go
+++ problems/transpose-matrix/transpose_matrix.go
@@ -1,1 +1,15 @@
 package transpose_matrix
+
+func transpose(A [][]int) [][]int {
+	m, n := len(A[0]), len(A)
+	B := make([][]int, m)
+	for i := 0; i < m; i++ {
+		B[i] = make([]int, n)
+	}
+	for i, row := range A {
+		for j, v := range row {
+			B[j][i] = v
+		}
+	}
+	return B
+}
