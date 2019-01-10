01684f04133828893088acf646f040311d67b438
Openset <openset@users.noreply.github.com>
2019-01-10 15:44:30 +0800

    Add: Reshape the Matrix

--- problems/reshape-the-matrix/reshape_the_matrix.go
+++ problems/reshape-the-matrix/reshape_the_matrix.go
@@ -1,1 +1,23 @@
 package reshape_the_matrix
+
+func matrixReshape(nums [][]int, r int, c int) [][]int {
+	m, n := len(nums), len(nums[0])
+	if m*n != r*c {
+		return nums
+	}
+	ans := make([][]int, r)
+	for i := 0; i < r; i++ {
+		ans[i] = make([]int, c)
+	}
+	i, j := 0, 0
+	for _, row := range nums {
+		for _, v := range row {
+			ans[i][j] = v
+			if j++; j == c {
+				i++
+				j = 0
+			}
+		}
+	}
+	return ans
+}
