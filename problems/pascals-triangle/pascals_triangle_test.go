43ce82628784f48d25f8685bb369a543ca6e46c0
Openset <openset.wang@gmail.com>
2019-01-10 20:47:10 +0800

    Add: Pascal's Triangle

--- problems/pascals-triangle/pascals_triangle_test.go
+++ problems/pascals-triangle/pascals_triangle_test.go
@@ -1,1 +1,32 @@
 package pascals_triangle
+
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    int
+	expected [][]int
+}
+
+func TestGenerate(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input: 5,
+			expected: [][]int{
+				{1},
+				{1, 1},
+				{1, 2, 1},
+				{1, 3, 3, 1},
+				{1, 4, 6, 4, 1},
+			},
+		},
+	}
+	for _, tc := range tests {
+		output := generate(tc.input)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
