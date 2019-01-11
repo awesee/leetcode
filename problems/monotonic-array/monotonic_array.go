de52857387cd8f72494f972439211dba77bf0593
Openset <openset@users.noreply.github.com>
2019-01-11 10:50:05 +0800

    Add: Monotonic Array

--- problems/monotonic-array/monotonic_array.go
+++ problems/monotonic-array/monotonic_array.go
@@ -1,1 +1,13 @@
 package monotonic_array
+
+func isMonotonic(A []int) bool {
+	t, f := 0, 0
+	for i, v := range A[1:] {
+		if v > A[i] {
+			t++
+		} else if v < A[i] {
+			f++
+		}
+	}
+	return t == 0 || f == 0
+}
--- problems/monotonic-array/monotonic_array_test.go
+++ problems/monotonic-array/monotonic_array_test.go
@@ -1,1 +1,43 @@
 package monotonic_array
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected bool
+}
+
+func TestIsMonotonic(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2, 2, 3},
+			expected: true,
+		},
+		{
+			input:    []int{6, 5, 4, 4},
+			expected: true,
+		},
+		{
+			input:    []int{1, 3, 2},
+			expected: false,
+		},
+		{
+			input:    []int{1, 2, 4, 5},
+			expected: true,
+		},
+		{
+			input:    []int{1, 1, 1},
+			expected: true,
+		},
+		{
+			input:    []int{1, 2, 3, 3, 3, 2, 1},
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := isMonotonic(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
