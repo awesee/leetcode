4113aea8a5a4ad4d383f8674d52639f297346832
Openset <openset.wang@gmail.com>
2019-01-11 21:55:18 +0800

    Add: Best Time to Buy and Sell Stock

--- problems/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock.go
+++ problems/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock.go
@@ -1,1 +1,13 @@
 package best_time_to_buy_and_sell_stock
+
+func maxProfit(prices []int) int {
+	ans, min := 0, 0
+	for i, v := range prices {
+		if v < prices[min] {
+			min = i
+		} else if v-prices[min] > ans {
+			ans = v - prices[min]
+		}
+	}
+	return ans
+}
--- problems/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock_test.go
+++ problems/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock_test.go
@@ -1,1 +1,35 @@
 package best_time_to_buy_and_sell_stock
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestMaxProfit(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{7, 1, 5, 3, 6, 4},
+			expected: 5,
+		},
+		{
+			input:    []int{7, 6, 4, 3, 1},
+			expected: 0,
+		},
+		{
+			input:    []int{1, 1, 1, 1, 1},
+			expected: 0,
+		},
+		{
+			input:    []int{},
+			expected: 0,
+		},
+	}
+	for _, tc := range tests {
+		output := maxProfit(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
