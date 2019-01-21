0be572d23f37572513972a73c66317971529adb9
Openset <openset@users.noreply.github.com>
2019-01-21 10:55:23 +0800

    Add: Largest Perimeter Triangle

---
+++ problems/largest-perimeter-triangle/largest_perimeter_triangle.go
@@ -0,0 +1,13 @@
+package largest_perimeter_triangle
+
+import "sort"
+
+func largestPerimeter(A []int) int {
+	sort.Ints(A)
+	for i := len(A) - 3; i >= 0; i-- {
+		if A[i]+A[i+1] > A[i+2] {
+			return A[i] + A[i+1] + A[i+2]
+		}
+	}
+	return 0
+}
