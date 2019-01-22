94f317bc4fa892f453659f76bf993229f964fc48
Openset <openset@users.noreply.github.com>
2019-01-22 11:42:52 +0800

    Add: X of a Kind in a Deck of Cards

--- problems/x-of-a-kind-in-a-deck-of-cards/x_of_a_kind_in_a_deck_of_cards.go
+++ problems/x-of-a-kind-in-a-deck-of-cards/x_of_a_kind_in_a_deck_of_cards.go
@@ -1,1 +1,20 @@
 package x_of_a_kind_in_a_deck_of_cards
+
+func hasGroupsSizeX(deck []int) bool {
+	m, l := make(map[int]int), len(deck)
+	for _, v := range deck {
+		m[v]++
+	}
+flag:
+	for x := 2; x <= l; x++ {
+		if l%x == 0 {
+			for _, v := range m {
+				if v%x != 0 {
+					continue flag
+				}
+			}
+			return true
+		}
+	}
+	return false
+}
--- problems/x-of-a-kind-in-a-deck-of-cards/x_of_a_kind_in_a_deck_of_cards_test.go
+++ problems/x-of-a-kind-in-a-deck-of-cards/x_of_a_kind_in_a_deck_of_cards_test.go
@@ -1,1 +1,39 @@
 package x_of_a_kind_in_a_deck_of_cards
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected bool
+}
+
+func TestHasGroupsSizeX(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2, 3, 4, 4, 3, 2, 1},
+			expected: true,
+		},
+		{
+			input:    []int{1, 1, 1, 2, 2, 2, 3, 3},
+			expected: false,
+		},
+		{
+			input:    []int{1},
+			expected: false,
+		},
+		{
+			input:    []int{1, 1},
+			expected: true,
+		},
+		{
+			input:    []int{1, 1, 2, 2, 2, 2},
+			expected: true,
+		},
+	}
+	for _, tc := range tests {
+		output := hasGroupsSizeX(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
