10dadcc8960b7a877e3ed4ed59415b4fa4c119d6
Openset <openset@users.noreply.github.com>
2019-01-23 10:59:49 +0800

    Add: Image Smoother

--- problems/image-smoother/image_smoother.go
+++ problems/image-smoother/image_smoother.go
@@ -1,1 +1,22 @@
 package image_smoother
+
+func imageSmoother(M [][]int) [][]int {
+	r, c := len(M), len(M[0])
+	ans := make([][]int, r)
+	for i := 0; i < r; i++ {
+		ans[i] = make([]int, c)
+		for j := 0; j < c; j++ {
+			sum, count := 0, 0
+			for m := i - 1; m <= i+1; m++ {
+				for n := j - 1; n <= j+1; n++ {
+					if m >= 0 && n >= 0 && m < r && n < c {
+						sum += M[m][n]
+						count++
+					}
+				}
+			}
+			ans[i][j] = sum / count
+		}
+	}
+	return ans
+}
--- problems/image-smoother/image_smoother_test.go
+++ problems/image-smoother/image_smoother_test.go
@@ -1,1 +1,46 @@
 package image_smoother
+
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    [][]int
+	expected [][]int
+}
+
+func TestImageSmoother(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input: [][]int{
+				{1, 1, 1},
+				{1, 0, 1},
+				{1, 1, 1},
+			},
+			expected: [][]int{
+				{0, 0, 0},
+				{0, 0, 0},
+				{0, 0, 0},
+			},
+		},
+		{
+			input: [][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+			},
+			expected: [][]int{
+				{3, 3, 4},
+				{4, 5, 5},
+				{6, 6, 7},
+			},
+		},
+	}
+	for _, tc := range tests {
+		output := imageSmoother(tc.input)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
