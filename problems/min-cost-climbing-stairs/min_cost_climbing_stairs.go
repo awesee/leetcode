f7a31a2786674384e76054053b06792f7e557f1b
Openset <openset@users.noreply.github.com>
2019-01-24 14:14:57 +0800

    Add: Min Cost Climbing Stairs

--- problems/min-cost-climbing-stairs/min_cost_climbing_stairs.go
+++ problems/min-cost-climbing-stairs/min_cost_climbing_stairs.go
@@ -1,1 +1,17 @@
 package min_cost_climbing_stairs
+
+func minCostClimbingStairs(cost []int) int {
+	f1, f2, l := 0, 0, len(cost)-1
+	for i := l; i >= 0; i-- {
+		f0 := f1
+		if f2 < f1 {
+			f0 = f2
+		}
+		f0 += cost[i]
+		f1, f2 = f0, f1
+	}
+	if f1 <= f2 {
+		return f1
+	}
+	return f2
+}
