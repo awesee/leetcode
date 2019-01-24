79d03763929cbe4f9228ea83fa8d72ac06184d16
Openset <openset@users.noreply.github.com>
2019-01-24 10:17:11 +0800

    Add: Missing Number

--- problems/missing-number/missing_number_test.go
+++ problems/missing-number/missing_number_test.go
@@ -1,1 +1,31 @@
 package missing_number
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestMissingNumber(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{3, 0, 1},
+			expected: 2,
+		},
+		{
+			input:    []int{0, 1, 2, 4, 5, 6},
+			expected: 3,
+		},
+		{
+			input:    []int{9, 6, 4, 2, 3, 5, 7, 0, 1},
+			expected: 8,
+		},
+	}
+	for _, tc := range tests {
+		output := missingNumber(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
