3ecd2bc8fd143d3f636bba82a7230801e478911e
Openset <openset@users.noreply.github.com>
2019-01-24 11:27:59 +0800

    Add: Maximum Product of Three Numbers

--- problems/maximum-product-of-three-numbers/maximum_product_of_three_numbers_test.go
+++ problems/maximum-product-of-three-numbers/maximum_product_of_three_numbers_test.go
@@ -1,1 +1,31 @@
 package maximum_product_of_three_numbers
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestMaximumProduct(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2, 3},
+			expected: 6,
+		},
+		{
+			input:    []int{1, 2, 3, 4},
+			expected: 24,
+		},
+		{
+			input:    []int{-2, -1, 0, 2, 3},
+			expected: 6,
+		},
+	}
+	for _, tc := range tests {
+		output := maximumProduct(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
