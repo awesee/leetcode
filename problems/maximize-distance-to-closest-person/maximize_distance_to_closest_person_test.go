bf841d6ca7aa81d9f30c4851510f31f6187b7fb6
Openset <openset@users.noreply.github.com>
2019-01-25 11:49:01 +0800

    Add: Maximize Distance to Closest Person

--- problems/maximize-distance-to-closest-person/maximize_distance_to_closest_person_test.go
+++ problems/maximize-distance-to-closest-person/maximize_distance_to_closest_person_test.go
@@ -1,1 +1,32 @@
 package maximize_distance_to_closest_person
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestMaxDistToClosest(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 0, 0, 0, 1, 0, 1},
+			expected: 2,
+		},
+		{
+			input:    []int{1, 0, 0, 0},
+			expected: 3,
+		},
+
+		{
+			input:    []int{0, 0, 0, 1},
+			expected: 3,
+		},
+	}
+	for _, tc := range tests {
+		output := maxDistToClosest(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
