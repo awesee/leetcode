4d538bf008928dc98a55f43cfdfa1da860940ece
Openset <openset@users.noreply.github.com>
2019-01-25 10:11:51 +0800

    Add: Longest Continuous Increasing Subsequence

--- problems/longest-continuous-increasing-subsequence/longest_continuous_increasing_subsequence.go
+++ problems/longest-continuous-increasing-subsequence/longest_continuous_increasing_subsequence.go
@@ -1,1 +1,16 @@
 package longest_continuous_increasing_subsequence
+
+func findLengthOfLCIS(nums []int) int {
+	max, cur := 0, 1
+	for i, v := range nums {
+		if i >= 1 && v > nums[i-1] {
+			cur++
+		} else {
+			cur = 1
+		}
+		if cur > max {
+			max = cur
+		}
+	}
+	return max
+}
--- problems/longest-continuous-increasing-subsequence/longest_continuous_increasing_subsequence_test.go
+++ problems/longest-continuous-increasing-subsequence/longest_continuous_increasing_subsequence_test.go
@@ -1,1 +1,35 @@
 package longest_continuous_increasing_subsequence
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestFindLengthOfLCIS(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 3, 5, 4, 7},
+			expected: 3,
+		},
+		{
+			input:    []int{2, 2, 2, 2, 2},
+			expected: 1,
+		},
+		{
+			input:    []int{},
+			expected: 0,
+		},
+		{
+			input:    []int{1, 3, 5, 7},
+			expected: 4,
+		},
+	}
+	for _, tc := range tests {
+		output := findLengthOfLCIS(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
