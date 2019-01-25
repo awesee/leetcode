bc7537157d3c222284cce8b5cf41d8ba00058812
Openset <openset@users.noreply.github.com>
2019-01-25 10:29:29 +0800

    Add: Find Pivot Index

--- problems/find-pivot-index/find_pivot_index.go
+++ problems/find-pivot-index/find_pivot_index.go
@@ -1,1 +1,17 @@
 package find_pivot_index
+
+func pivotIndex(nums []int) int {
+	left, right := 0, 0
+	for _, v := range nums {
+		right += v
+	}
+	for i, v := range nums {
+		right -= v
+		if left == right {
+			return i
+		} else {
+			left += v
+		}
+	}
+	return -1
+}
--- problems/find-pivot-index/find_pivot_index_test.go
+++ problems/find-pivot-index/find_pivot_index_test.go
@@ -1,1 +1,39 @@
 package find_pivot_index
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestPivotIndex(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 7, 3, 6, 5, 6},
+			expected: 3,
+		},
+		{
+			input:    []int{1, 2, 3},
+			expected: -1,
+		},
+		{
+			input:    []int{0},
+			expected: 0,
+		},
+		{
+			input:    []int{0, 0},
+			expected: 0,
+		},
+		{
+			input:    []int{1, 2, 1},
+			expected: 1,
+		},
+	}
+	for _, tc := range tests {
+		output := pivotIndex(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
