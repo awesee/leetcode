4a5a764a647ab0fa3e4daa37df7645abce4deb65
Openset <openset.wang@gmail.com>
2019-01-25 21:39:37 +0800

    Add: Largest Number At Least Twice of Others

--- problems/largest-number-at-least-twice-of-others/largest_number_at_least_twice_of_others.go
+++ problems/largest-number-at-least-twice-of-others/largest_number_at_least_twice_of_others.go
@@ -1,1 +1,16 @@
 package largest_number_at_least_twice_of_others
+
+func dominantIndex(nums []int) int {
+	ans, sec := 0, 0
+	for i, v := range nums {
+		if v > nums[ans] {
+			ans, sec = i, nums[ans]
+		} else if v > sec && i != ans {
+			sec = v
+		}
+	}
+	if nums[ans] >= 2*sec {
+		return ans
+	}
+	return -1
+}
--- problems/largest-number-at-least-twice-of-others/largest_number_at_least_twice_of_others_test.go
+++ problems/largest-number-at-least-twice-of-others/largest_number_at_least_twice_of_others_test.go
@@ -1,1 +1,39 @@
 package largest_number_at_least_twice_of_others
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestDominantIndex(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{3, 6, 1, 0},
+			expected: 1,
+		},
+		{
+			input:    []int{1, 2, 3, 4},
+			expected: -1,
+		},
+		{
+			input:    []int{1},
+			expected: 0,
+		},
+		{
+			input:    []int{0, 0, 3, 2},
+			expected: -1,
+		},
+		{
+			input:    []int{3, 0, 0, 2},
+			expected: -1,
+		},
+	}
+	for _, tc := range tests {
+		output := dominantIndex(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
