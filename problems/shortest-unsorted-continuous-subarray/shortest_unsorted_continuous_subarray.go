4cd398cddb1e4d82b6cc063b164d848e4dce9d7a
Openset <openset.wang@gmail.com>
2019-01-27 10:06:29 +0800

    Add: Shortest Unsorted Continuous Subarray

--- problems/shortest-unsorted-continuous-subarray/shortest_unsorted_continuous_subarray.go
+++ problems/shortest-unsorted-continuous-subarray/shortest_unsorted_continuous_subarray.go
@@ -1,1 +1,18 @@
 package shortest_unsorted_continuous_subarray
+
+func findUnsortedSubarray(nums []int) int {
+	m, n, l := 0, -1, len(nums)
+	for max, min, i, j := 0, l-1, 1, l-2; i < l; i, j = i+1, j-1 {
+		if nums[i] < nums[max] {
+			n = i
+		} else if nums[i] > nums[max] {
+			max = i
+		}
+		if nums[j] > nums[min] {
+			m = j
+		} else if nums[j] < nums[min] {
+			min = j
+		}
+	}
+	return n - m + 1
+}
