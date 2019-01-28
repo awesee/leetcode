66ef384da16e68560a51e5471a240bb16f1220e1
Openset <openset@users.noreply.github.com>
2019-01-28 10:25:36 +0800

    Update: reformat code

--- problems/big-countries/big_countries.sql
+++ problems/big-countries/big_countries.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 
 SELECT
-  `name`,
-  `population`,
-  `area`
+	`name`,
+	`population`,
+	`area`
 FROM
-  `World`
+	`World`
 WHERE
-  `area` > 3000000
-  OR `population` > 25000000
+	`area` > 3000000
+	OR `population` > 25000000;
