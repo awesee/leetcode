96b69a7b940301c635666b26b5ef06f7ae10d773
Openset <openset@users.noreply.github.com>
2019-01-28 15:52:04 +0800

    Add: Range Sum Query - Immutable

--- problems/range-sum-query-immutable/range_sum_query_immutable_test.go
+++ problems/range-sum-query-immutable/range_sum_query_immutable_test.go
@@ -1,1 +1,37 @@
 package range_sum_query_immutable
+
+import "testing"
+
+type caseType struct {
+	i        int
+	j        int
+	expected int
+}
+
+func TestConstructor(t *testing.T) {
+	nums := []int{-2, 0, 3, -5, 2, -1}
+	obj := Constructor(nums)
+	tests := [...]caseType{
+		{
+			i:        0,
+			j:        2,
+			expected: 1,
+		},
+		{
+			i:        2,
+			j:        5,
+			expected: -1,
+		},
+		{
+			i:        0,
+			j:        5,
+			expected: -3,
+		},
+	}
+	for _, tc := range tests {
+		output := obj.SumRange(tc.i, tc.j)
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.i, tc.j, output, tc.expected)
+		}
+	}
+}
