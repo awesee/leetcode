1e01d6e850181b759a29e587e6d069cd103e31c5
Openset <openset@users.noreply.github.com>
2019-01-29 10:38:04 +0800

    Add: House Robber

--- problems/house-robber/house_robber_test.go
+++ problems/house-robber/house_robber_test.go
@@ -1,1 +1,27 @@
 package house_robber
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestRob(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2, 3, 1},
+			expected: 4,
+		},
+		{
+			input:    []int{2, 7, 9, 3, 1},
+			expected: 12,
+		},
+	}
+	for _, tc := range tests {
+		output := rob(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
