ddc0f95a053fb846537b0e7338b2167ec7230cd8
Openset <openset@users.noreply.github.com>
2019-02-12 11:23:33 +0800

    Add: Merge Two Binary Trees

--- problems/merge-two-binary-trees/merge_two_binary_trees.go
+++ problems/merge-two-binary-trees/merge_two_binary_trees.go
@@ -1,1 +1,24 @@
 package merge_two_binary_trees
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1 == nil {
+		return t2
+	}
+	if t2 == nil {
+		return t1
+	}
+	t1.Val += t2.Val
+	t1.Left = mergeTrees(t1.Left, t2.Left)
+	t1.Right = mergeTrees(t1.Right, t2.Right)
+	return t1
+}
--- problems/merge-two-binary-trees/merge_two_binary_trees_test.go
+++ problems/merge-two-binary-trees/merge_two_binary_trees_test.go
@@ -1,1 +1,31 @@
 package merge_two_binary_trees
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/openset/leetcode/internal/kit"
+)
+
+type caseType struct {
+	t1       []int
+	t2       []int
+	expected []int
+}
+
+func TestMergeTrees(t *testing.T) {
+	tests := [...]caseType{
+		{
+			t1:       []int{1, 3, 2, 5},
+			t2:       []int{2, 1, 3, NULL, 4, NULL, 7},
+			expected: []int{3, 4, 5, 5, 4, NULL, 7},
+		},
+	}
+	for _, tc := range tests {
+		t3 := mergeTrees(SliceInt2TreeNode(tc.t1), SliceInt2TreeNode(tc.t2))
+		output := TreeNode2SliceInt(t3)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.t1, tc.t2, output, tc.expected)
+		}
+	}
+}
