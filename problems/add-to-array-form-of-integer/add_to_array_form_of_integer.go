63e2b54e8c2c47c29ec8bdf9c63ef1f4ad16fa72
openset <openset@users.noreply.github.com>
2019-02-13 14:14:52 +0800

    Add: Add to Array-Form of Integer

---
+++ problems/add-to-array-form-of-integer/add_to_array_form_of_integer.go
@@ -0,0 +1,14 @@
+package add_to_array_form_of_integer
+
+func addToArrayForm(A []int, K int) []int {
+	ans, carry, l := make([]int, 0), 0, len(A)-1
+	for carry > 0 || K > 0 || l >= 0 {
+		x := K%10 + carry
+		if l >= 0 {
+			x, l = x+A[l], l-1
+		}
+		carry, K = x/10, K/10
+		ans = append([]int{x % 10}, ans...)
+	}
+	return ans
+}
