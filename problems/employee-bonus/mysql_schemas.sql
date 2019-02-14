714017171b7799b14d961c8df742efe5c0aa4ceb
openset <openset@users.noreply.github.com>
2019-02-14 17:07:21 +0800

    Add: mysql schemas

---
+++ problems/employee-bonus/mysql_schemas.sql
@@ -0,0 +1,10 @@
+Create table If Not Exists Employee (EmpId int, Name varchar(255), Supervisor int, Salary int);
+Create table If Not Exists Bonus (EmpId int, Bonus int);
+Truncate table Employee;
+insert into Employee (EmpId, Name, Supervisor, Salary) values ('3', 'Brad', 'None', '4000');
+insert into Employee (EmpId, Name, Supervisor, Salary) values ('1', 'John', '3', '1000');
+insert into Employee (EmpId, Name, Supervisor, Salary) values ('2', 'Dan', '3', '2000');
+insert into Employee (EmpId, Name, Supervisor, Salary) values ('4', 'Thomas', '3', '4000');
+Truncate table Bonus;
+insert into Bonus (EmpId, Bonus) values ('2', '500');
+insert into Bonus (EmpId, Bonus) values ('4', '2000');
---
+++ problems/exchange-seats/mysql_schemas.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists seat(id int, student varchar(255));
+Truncate table seat;
+insert into seat (id, student) values ('1', 'Abbot');
+insert into seat (id, student) values ('2', 'Doris');
+insert into seat (id, student) values ('3', 'Emerson');
+insert into seat (id, student) values ('4', 'Green');
+insert into seat (id, student) values ('5', 'Jeames');
