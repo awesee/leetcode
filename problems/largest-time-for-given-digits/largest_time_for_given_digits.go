1af1aaf5396271fc2b282d3523a90b5541037821
openset <openset.wang@gmail.com>
2019-02-17 10:08:14 +0800

    Add: Largest Time for Given Digits

--- problems/largest-time-for-given-digits/largest_time_for_given_digits.go
+++ problems/largest-time-for-given-digits/largest_time_for_given_digits.go
@@ -1,1 +1,25 @@
 package largest_time_for_given_digits
+
+import (
+	"fmt"
+	"sort"
+)
+
+func largestTimeFromDigits(A []int) string {
+	sort.Ints(A)
+	for i := 3; i >= 0; i-- {
+		if A[i] < 3 {
+			for j := 3; j >= 0; j-- {
+				if j != i && A[i]*10+A[j] < 24 {
+					for k := 3; k >= 0; k-- {
+						if k != i && k != j && A[k] < 6 {
+							l := 6 - i - j - k
+							return fmt.Sprintf("%d%d:%d%d", A[i], A[j], A[k], A[l])
+						}
+					}
+				}
+			}
+		}
+	}
+	return ""
+}
--- problems/largest-time-for-given-digits/largest_time_for_given_digits_test.go
+++ problems/largest-time-for-given-digits/largest_time_for_given_digits_test.go
@@ -1,1 +1,31 @@
 package largest_time_for_given_digits
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected string
+}
+
+func TestLargestTimeFromDigits(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2, 3, 4},
+			expected: "23:41",
+		},
+		{
+			input:    []int{5, 5, 5, 5},
+			expected: "",
+		},
+		{
+			input:    []int{2, 0, 6, 6},
+			expected: "06:26",
+		},
+	}
+	for _, tc := range tests {
+		output := largestTimeFromDigits(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
