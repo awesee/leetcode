cb27df4d6761235fd864cf8be6e0e391ddde4ba7
openset <openset@users.noreply.github.com>
2019-02-19 10:39:24 +0800

    Add: Remove Duplicates from Sorted List

--- problems/remove-duplicates-from-sorted-list/remove_duplicates_from_sorted_list.go
+++ problems/remove-duplicates-from-sorted-list/remove_duplicates_from_sorted_list.go
@@ -1,1 +1,21 @@
 package remove_duplicates_from_sorted_list
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	for prev, t := head, head; t != nil; t = t.Next {
+		if prev.Val == t.Val {
+			prev.Next = t.Next
+		} else {
+			prev = t
+		}
+	}
+	return head
+}
--- problems/remove-duplicates-from-sorted-list/remove_duplicates_from_sorted_list_test.go
+++ problems/remove-duplicates-from-sorted-list/remove_duplicates_from_sorted_list_test.go
@@ -1,1 +1,36 @@
 package remove_duplicates_from_sorted_list
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/openset/leetcode/internal/kit"
+)
+
+type caseType struct {
+	input    []int
+	expected []int
+}
+
+func TestDeleteDuplicates(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 1, 2},
+			expected: []int{1, 2},
+		},
+		{
+			input:    []int{1, 1, 2, 3, 3},
+			expected: []int{1, 2, 3},
+		},
+		{
+			input:    []int{0, 0, 1, 1, 2, 2},
+			expected: []int{0, 1, 2},
+		},
+	}
+	for _, tc := range tests {
+		output := deleteDuplicates(SliceInt2ListNode(tc.input))
+		if !reflect.DeepEqual(ListNode2SliceInt(output), tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
