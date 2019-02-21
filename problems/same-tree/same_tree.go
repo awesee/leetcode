53af4bd234097b9521899fe82fe9b375bdcdc5f2
openset <openset@users.noreply.github.com>
2019-02-21 09:59:12 +0800

    Add: Same Tree

--- problems/same-tree/same_tree.go
+++ problems/same-tree/same_tree.go
@@ -1,1 +1,20 @@
 package same_tree
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil {
+		return q == nil
+	} else if q == nil || p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
