82cd8344a0e9445d1e70480e065683acae754c8a
openset <openset.wang@gmail.com>
2019-02-23 17:25:32 +0800

    Add: Reverse Linked List

--- problems/reverse-linked-list/reverse_linked_list.go
+++ problems/reverse-linked-list/reverse_linked_list.go
@@ -1,1 +1,18 @@
 package reverse_linked_list
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	for head != nil {
+		pre, head, head.Next = head, head.Next, pre
+	}
+	return pre
+}
