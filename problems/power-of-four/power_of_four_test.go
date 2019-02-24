86c8a3f0d56523447ebb79e6f6148be77ec66509
openset <openset.wang@gmail.com>
2019-02-24 14:48:03 +0800

    Add: Power of Four

--- problems/power-of-four/power_of_four_test.go
+++ problems/power-of-four/power_of_four_test.go
@@ -1,1 +1,43 @@
 package power_of_four
+
+import "testing"
+
+type caseType struct {
+	input    int
+	expected bool
+}
+
+func TestIsPowerOfFour(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    16,
+			expected: true,
+		},
+		{
+			input:    5,
+			expected: false,
+		},
+		{
+			input:    6,
+			expected: false,
+		},
+		{
+			input:    12,
+			expected: false,
+		},
+		{
+			input:    0,
+			expected: false,
+		},
+		{
+			input:    1,
+			expected: true,
+		},
+	}
+	for _, tc := range tests {
+		output := isPowerOfFour(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
