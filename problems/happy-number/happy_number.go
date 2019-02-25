c04a3a9eb24bddcbdc060955dced2743060a4b4f
openset <openset@users.noreply.github.com>
2019-02-25 13:06:32 +0800

    Add: Happy Number

--- problems/happy-number/happy_number.go
+++ problems/happy-number/happy_number.go
@@ -1,1 +1,16 @@
 package happy_number
+
+func isHappy(n int) bool {
+	m := make(map[int]bool)
+	for n != 1 && !m[n] {
+		m[n] = true
+		sum := 0
+		for n > 0 {
+			x := n % 10
+			sum += x * x
+			n /= 10
+		}
+		n = sum
+	}
+	return n == 1
+}
