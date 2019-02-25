5328f9f0af4577badafedba566ff765d717ebcb6
openset <openset@users.noreply.github.com>
2019-02-25 10:45:30 +0800

    Add: Palindrome Linked List

--- problems/palindrome-linked-list/palindrome_linked_list.go
+++ problems/palindrome-linked-list/palindrome_linked_list.go
@@ -1,1 +1,28 @@
 package palindrome_linked_list
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+	var prev *ListNode
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	for slow != nil {
+		prev, slow, slow.Next = slow, slow.Next, prev
+	}
+	for head != nil && prev != nil {
+		if head.Val != prev.Val {
+			return false
+		}
+		head, prev = head.Next, prev.Next
+	}
+	return true
+}
--- problems/palindrome-linked-list/palindrome_linked_list_test.go
+++ problems/palindrome-linked-list/palindrome_linked_list_test.go
@@ -1,1 +1,35 @@
 package palindrome_linked_list
+
+import (
+	"testing"
+
+	. "github.com/openset/leetcode/internal/kit"
+)
+
+type caseType struct {
+	input    []int
+	expected bool
+}
+
+func TestIsPalindrome(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2},
+			expected: false,
+		},
+		{
+			input:    []int{1, 2, 1},
+			expected: true,
+		},
+		{
+			input:    []int{1, 2, 2, 1},
+			expected: true,
+		},
+	}
+	for _, tc := range tests {
+		output := isPalindrome(SliceInt2ListNode(tc.input))
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
