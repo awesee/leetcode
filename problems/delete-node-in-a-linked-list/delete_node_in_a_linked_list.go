206e7df794e503b8d3d41bae9f20ea7aded8bb6f
openset <openset@users.noreply.github.com>
2019-02-25 10:07:20 +0800

    Add: Delete Node in a Linked List

--- problems/delete-node-in-a-linked-list/delete_node_in_a_linked_list.go
+++ problems/delete-node-in-a-linked-list/delete_node_in_a_linked_list.go
@@ -1,1 +1,17 @@
 package delete_node_in_a_linked_list
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+	if node != nil && node.Next != nil {
+		node.Val = node.Next.Val
+		node.Next = node.Next.Next
+	}
+}
