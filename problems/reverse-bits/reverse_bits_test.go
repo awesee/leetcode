6a5f7a3eeb423d645d2092bf841ffc29b0589b1f
openset <openset@users.noreply.github.com>
2019-02-26 10:12:17 +0800

    Add: Reverse Bits

--- problems/reverse-bits/reverse_bits_test.go
+++ problems/reverse-bits/reverse_bits_test.go
@@ -1,1 +1,27 @@
 package reverse_bits
+
+import "testing"
+
+type caseType struct {
+	input    uint32
+	expected uint32
+}
+
+func TestReverseBits(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    43261596,
+			expected: 964176192,
+		},
+		{
+			input:    4294967293,
+			expected: 3221225471,
+		},
+	}
+	for _, tc := range tests {
+		output := reverseBits(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
