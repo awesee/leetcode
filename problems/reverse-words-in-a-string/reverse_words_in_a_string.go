d542d1a670e2af3c79634952ce5b5301f8018247
openset <openset@users.noreply.github.com>
2019-02-27 14:26:18 +0800

    Add: Reverse Words in a String

--- problems/reverse-words-in-a-string/reverse_words_in_a_string.go
+++ problems/reverse-words-in-a-string/reverse_words_in_a_string.go
@@ -1,1 +1,15 @@
 package reverse_words_in_a_string
+
+import (
+	"regexp"
+	"strings"
+)
+
+func reverseWords(s string) string {
+	reg := regexp.MustCompile(`\S+`)
+	words := reg.FindAllString(s, -1)
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+	return strings.Join(words, " ")
+}
