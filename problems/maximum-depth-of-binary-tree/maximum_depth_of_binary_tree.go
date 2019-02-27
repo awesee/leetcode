39f200bb45c675d0b817dd4a3f6923cb2e8c4848
openset <openset@users.noreply.github.com>
2019-02-27 10:23:18 +0800

    Add: Maximum Depth of Binary Tree

--- problems/maximum-depth-of-binary-tree/maximum_depth_of_binary_tree.go
+++ problems/maximum-depth-of-binary-tree/maximum_depth_of_binary_tree.go
@@ -1,1 +1,23 @@
 package maximum_depth_of_binary_tree
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := maxDepth(root.Left) + 1
+	r := maxDepth(root.Right) + 1
+	if l > r {
+		return l
+	}
+	return r
+}
