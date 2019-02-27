8f547278ce150d0a7102109e6a0d5b9f049463c7
openset <openset@users.noreply.github.com>
2019-02-27 12:02:36 +0800

    Add: Min Stack

--- problems/min-stack/min_stack.go
+++ problems/min-stack/min_stack.go
@@ -1,1 +1,39 @@
 package min_stack
+
+type MinStack struct {
+	stack [][2]int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(x int) {
+	min, l := x, len(this.stack)
+	if l > 0 && this.stack[l-1][1] < x {
+		min = this.stack[l-1][1]
+	}
+	this.stack = append(this.stack, [2]int{x, min})
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1][0]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.stack[len(this.stack)-1][1]
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
