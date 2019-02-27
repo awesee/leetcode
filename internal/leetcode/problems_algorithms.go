f489a41512cf7784af3f544a09cecfbd0ae79456
openset <openset@users.noreply.github.com>
2019-02-27 09:17:54 +0800

    Update: cache time

--- internal/leetcode/problems_algorithms.go
+++ internal/leetcode/problems_algorithms.go
@@ -3,7 +3,7 @@ package leetcode
 import "github.com/openset/leetcode/internal/client"
 
 func ProblemsAlgorithms() (ps problemsType) {
-	data := remember(problemsAlgorithmsFile, 3, func() []byte {
+	data := remember(problemsAlgorithmsFile, 2, func() []byte {
 		return client.Get(apiProblemsAlgorithmsUrl)
 	})
 	jsonDecode(data, &ps)
--- internal/leetcode/problems_shell.go
+++ internal/leetcode/problems_shell.go
@@ -3,7 +3,7 @@ package leetcode
 import "github.com/openset/leetcode/internal/client"
 
 func ProblemsShell() (ps problemsType) {
-	data := remember(problemsShellFile, 3, func() []byte {
+	data := remember(problemsShellFile, 2, func() []byte {
 		return client.Get(apiProblemsShellUrl)
 	})
 	jsonDecode(data, &ps)
