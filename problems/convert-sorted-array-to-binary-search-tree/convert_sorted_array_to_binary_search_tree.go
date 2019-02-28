f5893eae246a97be45607d2069b37d3ff3573156
openset <openset.wang@gmail.com>
2019-02-28 15:19:13 +0800

    Add: Convert Sorted Array to Binary Search Tree

--- problems/convert-sorted-array-to-binary-search-tree/convert_sorted_array_to_binary_search_tree.go
+++ problems/convert-sorted-array-to-binary-search-tree/convert_sorted_array_to_binary_search_tree.go
@@ -1,1 +1,21 @@
 package convert_sorted_array_to_binary_search_tree
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	var node *TreeNode
+	if l := len(nums); l > 0 {
+		node = &TreeNode{Val: nums[l/2]}
+		node.Left = sortedArrayToBST(nums[0 : l/2])
+		node.Right = sortedArrayToBST(nums[l/2+1:])
+	}
+	return node
+}
--- problems/convert-sorted-array-to-binary-search-tree/convert_sorted_array_to_binary_search_tree_test.go
+++ problems/convert-sorted-array-to-binary-search-tree/convert_sorted_array_to_binary_search_tree_test.go
@@ -1,1 +1,28 @@
 package convert_sorted_array_to_binary_search_tree
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/openset/leetcode/internal/kit"
+)
+
+type caseType struct {
+	input    []int
+	expected []int
+}
+
+func TestSortedArrayToBST(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{-10, -3, 0, 5, 9},
+			expected: []int{0, -3, 9, -10, 5},
+		},
+	}
+	for _, tc := range tests {
+		output := TreeNode2SliceInt(sortedArrayToBST(tc.input))
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
