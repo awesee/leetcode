4485d6329c714e0de9c5602894f3fb946cff5a26
openset <openset.wang@gmail.com>
2019-02-28 13:52:14 +0800

    Add: Available Captures for Rook

---
+++ problems/available-captures-for-rook/available_captures_for_rook.go
@@ -0,0 +1,33 @@
+package available_captures_for_rook
+
+func numRookCaptures(board [][]byte) int {
+	ans, rx, ry, left, up := 0, -1, -1, [8]int{}, [8]int{}
+	for i, row := range board {
+		for j, v := range row {
+			if rx == -1 {
+				if v == 'p' {
+					left[i], up[j] = 1, 1
+				} else if v == 'B' {
+					left[i], up[j] = 0, 0
+				} else if v == 'R' {
+					ans, rx, ry = left[i]+up[j], i, j
+				}
+			} else if i == rx {
+				if v == 'p' {
+					ans++
+				}
+				if v != '.' {
+					break
+				}
+			} else if j == ry {
+				if v == 'p' {
+					ans++
+				}
+				if v != '.' {
+					return ans
+				}
+			}
+		}
+	}
+	return ans
+}
