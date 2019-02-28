bcf535d11152d89a79d1c93cfc92b57fbd2f1a01
openset <openset.wang@gmail.com>
2019-02-28 15:46:41 +0800

    Add: Symmetric Tree

--- problems/symmetric-tree/symmetric_tree.go
+++ problems/symmetric-tree/symmetric_tree.go
@@ -1,1 +1,24 @@
 package symmetric_tree
+
+import . "github.com/openset/leetcode/internal/kit"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return isMirror(root, root)
+}
+
+func isMirror(t1, t2 *TreeNode) bool {
+	if t1 == nil && t2 == nil {
+		return true
+	} else if t1 == nil || t2 == nil {
+		return false
+	}
+	return t1.Val == t2.Val && isMirror(t1.Left, t2.Right) && isMirror(t1.Right, t2.Left)
+}
--- problems/symmetric-tree/symmetric_tree_test.go
+++ problems/symmetric-tree/symmetric_tree_test.go
@@ -1,1 +1,31 @@
 package symmetric_tree
+
+import (
+	"testing"
+
+	. "github.com/openset/leetcode/internal/kit"
+)
+
+type caseType struct {
+	input    []int
+	expected bool
+}
+
+func TestIsSymmetric(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2, 2, 3, 4, 4, 3},
+			expected: true,
+		},
+		{
+			input:    []int{1, 2, 2, NULL, 3, NULL, 3},
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := isSymmetric(SliceInt2TreeNode(tc.input))
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
