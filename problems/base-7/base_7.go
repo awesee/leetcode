8fb4089f1340e126d21e539d93ef5993eba885ae
openset <openset.wang@gmail.com>
2019-04-05 10:00:39 +0800

    Add: Base 7

--- problems/base-7/base_7.go
+++ problems/base-7/base_7.go
@@ -1,1 +1,12 @@
 package base_7
+
+func convertToBase7(num int) string {
+	ans := ""
+	if num < 0 {
+		ans, num = "-", -num
+	}
+	if num >= 7 {
+		ans += convertToBase7(num / 7)
+	}
+	return ans + string('0'+num%7)
+}
