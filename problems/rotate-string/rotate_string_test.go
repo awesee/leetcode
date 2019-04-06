32870070fe1dc4e620bf894e0c237104130df56c
openset <openset.wang@gmail.com>
2019-04-06 12:58:58 +0800

    Add: Rotate String

--- problems/rotate-string/rotate_string_test.go
+++ problems/rotate-string/rotate_string_test.go
@@ -1,1 +1,30 @@
 package rotate_string
+
+import "testing"
+
+type caseType struct {
+	A        string
+	B        string
+	expected bool
+}
+
+func TestRotateString(t *testing.T) {
+	tests := [...]caseType{
+		{
+			A:        "abcde",
+			B:        "cdeab",
+			expected: true,
+		},
+		{
+			A:        "abcde",
+			B:        "abced",
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := rotateString(tc.A, tc.B)
+		if output != tc.expected {
+			t.Fatalf("input: %v %v, output: %v, expected: %v", tc.A, tc.B, output, tc.expected)
+		}
+	}
+}
