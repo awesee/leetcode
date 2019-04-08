d6fb4097755aabcffdc650e5f7f738916e601544
openset <openset.wang@gmail.com>
2019-04-08 10:04:07 +0800

    Add: Remove Outermost Parentheses

---
+++ problems/remove-outermost-parentheses/remove_outermost_parentheses.go
@@ -0,0 +1,16 @@
+package remove_outermost_parentheses
+
+func removeOuterParentheses(S string) string {
+	ans, n := make([]rune, 0, len(S)), 0
+	for _, c := range S {
+		if c == '(' {
+			n++
+		} else {
+			n--
+		}
+		if (c == '(' && n != 1) || (c == ')' && n != 0) {
+			ans = append(ans, c)
+		}
+	}
+	return string(ans)
+}
