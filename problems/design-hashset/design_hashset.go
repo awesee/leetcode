ad93bfbe7bf025c74667ebf0acbc6f2aedfd8d4b
openset <openset.wang@gmail.com>
2019-05-15 14:14:28 +0800

    Add: Design HashSet

--- problems/design-hashset/design_hashset.go
+++ problems/design-hashset/design_hashset.go
@@ -1,1 +1,31 @@
 package design_hashset
+
+type MyHashSet struct {
+	data map[int]bool
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyHashSet {
+	return MyHashSet{make(map[int]bool, 0)}
+}
+
+func (this *MyHashSet) Add(key int) {
+	this.data[key] = true
+}
+
+func (this *MyHashSet) Remove(key int) {
+	delete(this.data, key)
+}
+
+/** Returns true if this set contains the specified element */
+func (this *MyHashSet) Contains(key int) bool {
+	return this.data[key]
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
--- problems/design-hashset/design_hashset_test.go
+++ problems/design-hashset/design_hashset_test.go
@@ -1,1 +1,18 @@
 package design_hashset
+
+import "testing"
+
+func TestDesignHashset(t *testing.T) {
+	obj := Constructor()
+	obj.Add(1)
+	obj.Add(2)
+	output := obj.Contains(1)
+	output = !obj.Contains(3) && output
+	obj.Add(2)
+	output = obj.Contains(2) && output
+	obj.Remove(2)
+	output = !obj.Contains(2) && output
+	if !output {
+		t.Fatalf("output: %v, expected: %v", output, true)
+	}
+}
