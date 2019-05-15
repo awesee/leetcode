6ab6741c205e7dbad6fae8202f643c47cc56823b
openset <openset.wang@gmail.com>
2019-05-15 14:34:32 +0800

    Add: Design HashMap

--- problems/design-hashmap/design_hashmap.go
+++ problems/design-hashmap/design_hashmap.go
@@ -1,1 +1,36 @@
 package design_hashmap
+
+type MyHashMap struct {
+	data map[int]int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyHashMap {
+	return MyHashMap{make(map[int]int, 0)}
+}
+
+/** value will always be non-negative. */
+func (this *MyHashMap) Put(key int, value int) {
+	this.data[key] = value
+}
+
+/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+func (this *MyHashMap) Get(key int) int {
+	if v, ok := this.data[key]; ok {
+		return v
+	}
+	return -1
+}
+
+/** Removes the mapping of the specified value key if this map contains a mapping for the key */
+func (this *MyHashMap) Remove(key int) {
+	delete(this.data, key)
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Put(key,value);
+ * param_2 := obj.Get(key);
+ * obj.Remove(key);
+ */
