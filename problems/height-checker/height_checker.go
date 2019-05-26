24e30f9f57717972d4946a55e4efd095aa37f827
openset <openset.wang@gmail.com>
2019-05-26 14:52:55 +0800

    Add: Height Checker

---
+++ problems/height-checker/height_checker.go
@@ -0,0 +1,15 @@
+package height_checker
+
+import "sort"
+
+func heightChecker(heights []int) int {
+	ans, dst := 0, make([]int, len(heights))
+	copy(dst, heights)
+	sort.Ints(dst)
+	for i, v := range dst {
+		if heights[i] != v {
+			ans++
+		}
+	}
+	return ans
+}
