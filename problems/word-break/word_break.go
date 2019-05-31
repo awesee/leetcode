c59b2c89070cb3e93a95be4130c1aa62dc470a47
openset <openset.wang@gmail.com>
2019-05-31 14:01:25 +0800

    Add: Word Break

--- problems/word-break/word_break.go
+++ problems/word-break/word_break.go
@@ -1,1 +1,19 @@
 package word_break
+
+func wordBreak(s string, wordDict []string) bool {
+	wm, l := make(map[string]bool), len(s)
+	for _, w := range wordDict {
+		wm[w] = true
+	}
+	dp := make([]bool, l+1)
+	dp[0] = true
+	for i := 1; i <= l; i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && wm[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[l]
+}
--- problems/word-break/word_break_test.go
+++ problems/word-break/word_break_test.go
@@ -1,1 +1,35 @@
 package word_break
+
+import "testing"
+
+type caseType struct {
+	input    string
+	wordDict []string
+	expected bool
+}
+
+func TestWordBreak(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "leetcode",
+			wordDict: []string{"leet", "code"},
+			expected: true,
+		},
+		{
+			input:    "applepenapple",
+			wordDict: []string{"apple", "pen"},
+			expected: true,
+		},
+		{
+			input:    "catsandog",
+			wordDict: []string{"cats", "dog", "sand", "and", "cat"},
+			expected: false,
+		},
+	}
+	for _, tc := range tests {
+		output := wordBreak(tc.input, tc.wordDict)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
