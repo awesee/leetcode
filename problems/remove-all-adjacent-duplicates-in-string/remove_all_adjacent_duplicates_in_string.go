0f5b36c1205108b23ed7c57fc6f88fa786f7d3ab
openset <openset.wang@gmail.com>
2019-05-31 11:36:11 +0800

    Add: Remove All Adjacent Duplicates In String

--- problems/remove-all-adjacent-duplicates-in-string/remove_all_adjacent_duplicates_in_string.go
+++ problems/remove-all-adjacent-duplicates-in-string/remove_all_adjacent_duplicates_in_string.go
@@ -3,9 +3,9 @@ package remove_all_adjacent_duplicates_in_string
 func removeDuplicates(S string) string {
 	ans := make([]byte, 0, len(S))
 	for i := 0; i < len(S); i++ {
-		n := len(ans)
-		if n >= 1 && ans[n-1] == S[i] {
-			ans = ans[:n-1]
+		n := len(ans) - 1
+		if n >= 0 && ans[n] == S[i] {
+			ans = ans[:n]
 		} else {
 			ans = append(ans, S[i])
 		}
