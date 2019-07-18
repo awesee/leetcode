75fbbbd2fa132f490ee5d48a76641080f3aec78d
openset <openset.wang@gmail.com>
2019-07-18 11:07:22 +0800

    Add: 3Sum Closest

--- problems/3sum-closest/3sum_closest_test.go
+++ problems/3sum-closest/3sum_closest_test.go
@@ -1,1 +1,25 @@
 package p_3sum_closest
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	target   int
+	expected int
+}
+
+func TestThreeSumClosest(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{-1, 2, 1, -4},
+			target:   1,
+			expected: 2,
+		},
+	}
+	for _, tc := range tests {
+		output := threeSumClosest(tc.input, tc.target)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
