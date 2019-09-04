21b0db5f0f5d7660b8e80181593189f6b6ce8d2c
openset <openset.wang@gmail.com>
2019-09-04 12:03:43 +0800

    Add: Letter Combinations of a Phone Number

--- problems/letter-combinations-of-a-phone-number/letter_combinations_of_a_phone_number_test.go
+++ problems/letter-combinations-of-a-phone-number/letter_combinations_of_a_phone_number_test.go
@@ -1,1 +1,30 @@
 package letter_combinations_of_a_phone_number
+
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    string
+	expected []string
+}
+
+func TestLetterCombinations(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "",
+			expected: nil,
+		},
+		{
+			input:    "23",
+			expected: []string{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"},
+		},
+	}
+	for _, tc := range tests {
+		output := letterCombinations(tc.input)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
