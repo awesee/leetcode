df1e8c3f8b25af61999c8119e754720eef892f9e
openset <openset.wang@gmail.com>
2019-09-14 16:24:32 +0800

    Add: test

--- problems/01-matrix/01_matrix_test.go
+++ problems/01-matrix/01_matrix_test.go
@@ -1,1 +1,46 @@
 package p_01_matrix
+
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    [][]int
+	expected [][]int
+}
+
+func TestUpdateMatrix(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input: [][]int{
+				{0, 0, 0},
+				{0, 1, 0},
+				{0, 0, 0},
+			},
+			expected: [][]int{
+				{0, 0, 0},
+				{0, 1, 0},
+				{0, 0, 0},
+			},
+		},
+		{
+			input: [][]int{
+				{0, 0, 0},
+				{0, 1, 0},
+				{1, 1, 1},
+			},
+			expected: [][]int{
+				{0, 0, 0},
+				{0, 1, 0},
+				{1, 2, 1},
+			},
+		},
+	}
+	for _, tc := range tests {
+		output := updateMatrix(tc.input)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
