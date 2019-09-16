0484185ec8d999438c80f4bb8c465080fffdc211
openset <openset.wang@gmail.com>
2019-09-16 15:01:57 +0800

    Add: 4Sum II

--- problems/4sum-ii/4sum_ii.go
+++ problems/4sum-ii/4sum_ii.go
@@ -1,1 +1,17 @@
 package p_4sum_ii
+
+func fourSumCount(A []int, B []int, C []int, D []int) int {
+	ans, n := 0, len(A)
+	sum := make(map[int]int, n*n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			sum[A[i]+B[j]]++
+		}
+	}
+	for k := 0; k < n; k++ {
+		for l := 0; l < n; l++ {
+			ans += sum[-C[k]-D[l]]
+		}
+	}
+	return ans
+}
