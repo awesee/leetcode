53cb78ed12e634aaa881bc5048927515b5989180
Shuo <openset.wang@gmail.com>
2019-09-17 10:10:28 +0800

    Add: 132 Pattern

--- problems/132-pattern/132_pattern_test.go
+++ problems/132-pattern/132_pattern_test.go
@@ -1,1 +1,31 @@
 package p_132_pattern
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected bool
+}
+
+func TestFind132pattern(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 2, 3, 4},
+			expected: false,
+		},
+		{
+			input:    []int{3, 1, 4, 2},
+			expected: true,
+		},
+		{
+			input:    []int{-1, 3, 2, 0},
+			expected: true,
+		},
+	}
+	for _, tc := range tests {
+		output := find132pattern(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
