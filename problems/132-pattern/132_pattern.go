4eef01735222ca4429eeace43d91d3ba7fbe2f4c
openset <openset.wang@gmail.com>
2019-09-17 09:55:07 +0800

    Add: 132 Pattern

--- problems/132-pattern/132_pattern.go
+++ problems/132-pattern/132_pattern.go
@@ -1,1 +1,16 @@
 package p_132_pattern
+
+func find132pattern(nums []int) bool {
+	ak, ajStack := -1<<31, make([]int, 0, len(nums))
+	for i := len(nums) - 1; 0 <= i; i-- {
+		if nums[i] < ak {
+			return true
+		}
+		for len(ajStack) > 0 && ajStack[len(ajStack)-1] < nums[i] {
+			ak = ajStack[len(ajStack)-1]
+			ajStack = ajStack[:len(ajStack)-1]
+		}
+		ajStack = append(ajStack, nums[i])
+	}
+	return false
+}
