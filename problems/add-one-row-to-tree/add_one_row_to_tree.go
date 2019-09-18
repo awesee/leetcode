57bcc3904e4257b621cd93699c53cbf622b38460
openset <openset.wang@gmail.com>
2019-09-18 12:26:53 +0800

    Add: Add One Row to Tree

--- problems/add-one-row-to-tree/add_one_row_to_tree.go
+++ problems/add-one-row-to-tree/add_one_row_to_tree.go
@@ -1,1 +1,31 @@
 package add_one_row_to_tree
+
+import "github.com/openset/leetcode/internal/kit"
+
+type TreeNode = kit.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func addOneRow(root *TreeNode, v int, d int) *TreeNode {
+	switch d {
+	case 1:
+		return &TreeNode{Val: v, Left: root}
+	case 2:
+		root.Left = &TreeNode{Val: v, Left: root.Left}
+		root.Right = &TreeNode{Val: v, Right: root.Right}
+	default:
+		if root.Left != nil {
+			root.Left = addOneRow(root.Left, v, d-1)
+		}
+		if root.Right != nil {
+			root.Right = addOneRow(root.Right, v, d-1)
+		}
+	}
+	return root
+}
--- problems/add-one-row-to-tree/add_one_row_to_tree_test.go
+++ problems/add-one-row-to-tree/add_one_row_to_tree_test.go
@@ -1,1 +1,40 @@
 package add_one_row_to_tree
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/openset/leetcode/internal/kit"
+)
+
+type caseType struct {
+	input    []int
+	v        int
+	d        int
+	expected []int
+}
+
+func TestAddOneRow(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{4, 2, 6, 3, 1, 5},
+			v:        1,
+			d:        2,
+			expected: []int{4, 1, 1, 2, kit.NULL, kit.NULL, 6, 3, 1, 5},
+		},
+		{
+			input:    []int{4, 2, kit.NULL, 3, 1},
+			v:        1,
+			d:        3,
+			expected: []int{4, 2, kit.NULL, 1, 1, 3, kit.NULL, kit.NULL, 1},
+		},
+	}
+	for _, tc := range tests {
+		root := kit.SliceInt2TreeNode(tc.input)
+		root = addOneRow(root, tc.v, tc.d)
+		output := kit.TreeNode2SliceInt(root)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
