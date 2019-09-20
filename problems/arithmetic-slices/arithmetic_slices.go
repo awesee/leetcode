a97609bbc44767dc73b6ed137e94290b92ccc2bd
openset <openset.wang@gmail.com>
2019-09-20 10:38:06 +0800

    Add: Arithmetic Slices

--- problems/arithmetic-slices/arithmetic_slices.go
+++ problems/arithmetic-slices/arithmetic_slices.go
@@ -1,1 +1,14 @@
-package arithmetic_slices
+package problem_413
+
+func numberOfArithmeticSlices(A []int) int {
+	ans, i, j, l := 0, 0, 0, len(A)-1
+	for i < l-1 {
+		j = i + 1
+		for j < l && A[j+1]-A[j] == A[j]-A[j-1] {
+			j++
+		}
+		i, j = j, j-i
+		ans += j * (j - 1) / 2
+	}
+	return ans
+}
--- problems/arithmetic-slices/arithmetic_slices_test.go
+++ problems/arithmetic-slices/arithmetic_slices_test.go
@@ -1,1 +1,47 @@
-package arithmetic_slices
+package problem_413
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestNumberOfArithmeticSlices(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{1, 3, 5, 7, 9},
+			expected: 6,
+		},
+		{
+			input:    []int{7, 7, 7, 7},
+			expected: 3,
+		},
+		{
+			input:    []int{3, -1, -5, -9},
+			expected: 3,
+		},
+		{
+			input:    []int{1, 2, 3, 4},
+			expected: 3,
+		},
+		{
+			input:    []int{1, 2, 3, 4, 6, 8, 10},
+			expected: 6,
+		},
+		{
+			input:    []int{1, 2},
+			expected: 0,
+		},
+		{
+			input:    []int{1, 2, 4, 5, 7},
+			expected: 0,
+		},
+	}
+	for _, tc := range tests {
+		output := numberOfArithmeticSlices(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
