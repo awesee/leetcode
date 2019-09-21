54eb30db7a2cce57472279605f63e5d75b0d88fa
openset <openset.wang@gmail.com>
2019-09-21 16:58:43 +0800

    Add: Preimage Size of Factorial Zeroes Function

--- problems/preimage-size-of-factorial-zeroes-function/preimage_size_of_factorial_zeroes_function.go
+++ problems/preimage-size-of-factorial-zeroes-function/preimage_size_of_factorial_zeroes_function.go
@@ -1,1 +1,26 @@
-package preimage_size_of_factorial_zeroes_function
+package problem_793
+
+func preimageSizeFZF(K int) int {
+	l, r := 0, 5*(K+1)
+	for l <= r {
+		m := l + (r-l)/2
+		km := zeros(m)
+		if km < K {
+			l = m + 1
+		} else if km > K {
+			r = m - 1
+		} else {
+			return 5
+		}
+	}
+	return 0
+}
+
+func zeros(x int) int {
+	r := 0
+	for x > 0 {
+		r += x / 5
+		x /= 5
+	}
+	return r
+}
--- problems/preimage-size-of-factorial-zeroes-function/preimage_size_of_factorial_zeroes_function_test.go
+++ problems/preimage-size-of-factorial-zeroes-function/preimage_size_of_factorial_zeroes_function_test.go
@@ -1,1 +1,35 @@
-package preimage_size_of_factorial_zeroes_function
+package problem_793
+
+import "testing"
+
+type caseType struct {
+	input    int
+	expected int
+}
+
+func TestPreimageSizeFZF(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    0,
+			expected: 5,
+		},
+		{
+			input:    5,
+			expected: 0,
+		},
+		{
+			input:    17,
+			expected: 0,
+		},
+		{
+			input:    11,
+			expected: 0,
+		},
+	}
+	for _, tc := range tests {
+		output := preimageSizeFZF(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
