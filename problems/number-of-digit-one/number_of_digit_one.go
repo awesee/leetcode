c7e3bb24527eb4bfc3f24fee62abf4d478b5b520
openset <openset.wang@gmail.com>
2019-09-21 16:39:02 +0800

    Add: Number of Digit One

--- problems/number-of-digit-one/number_of_digit_one.go
+++ problems/number-of-digit-one/number_of_digit_one.go
@@ -1,1 +1,14 @@
-package number_of_digit_one
+package problem_233
+
+func countDigitOne(n int) int {
+	ans, b, x := 0, 1, 0
+	for b <= n {
+		x = n / b
+		ans += (x + 8) / 10 * b
+		if x%10 == 1 {
+			ans += n%b + 1
+		}
+		b *= 10
+	}
+	return ans
+}
