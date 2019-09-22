c17cc27f63e0266dd7d0c45ff416163c552e14e3
openset <openset.wang@gmail.com>
2019-09-22 10:36:49 +0800

    Add: Valid Number

--- problems/valid-number/valid_number.go
+++ problems/valid-number/valid_number.go
@@ -1,1 +1,12 @@
-package valid_number
+package problem_65
+
+import (
+	"strconv"
+	"strings"
+)
+
+func isNumber(s string) bool {
+	s = strings.TrimSpace(s)
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil || err.(*strconv.NumError).Err != strconv.ErrSyntax
+}
