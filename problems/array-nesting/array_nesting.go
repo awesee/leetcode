27e53260cf4134b953b91c97d3181dd882fa3e86
openset <openset.wang@gmail.com>
2019-09-26 11:55:14 +0800

    Add: Array Nesting

--- problems/array-nesting/array_nesting.go
+++ problems/array-nesting/array_nesting.go
@@ -1,1 +1,16 @@
-package array_nesting
+package problem_565
+
+func arrayNesting(nums []int) int {
+	ans := 0
+	for i := range nums {
+		c := 1
+		for nums[i] != i {
+			nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
+			c++
+		}
+		if ans < c {
+			ans = c
+		}
+	}
+	return ans
+}
--- problems/array-nesting/array_nesting_test.go
+++ problems/array-nesting/array_nesting_test.go
@@ -1,1 +1,37 @@
-package array_nesting
+package problem_565
+
+import (
+	"testing"
+)
+
+type caseType struct {
+	input    []int
+	expected int
+}
+
+func TestArrayNesting(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{5, 4, 0, 3, 1, 6, 2},
+			expected: 4,
+		},
+		{
+			input:    []int{0, 3, 1, 5, 4, 6, 2},
+			expected: 5,
+		},
+		{
+			input:    []int{6, 2, 5, 4, 0, 3, 1},
+			expected: 7,
+		},
+		{
+			input:    []int{3, 1, 5, 4, 0, 6, 2},
+			expected: 3,
+		},
+	}
+	for _, tc := range tests {
+		output := arrayNesting(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
