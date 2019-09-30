9c8349134695ddfd73392537579762a9cf91e642
openset <openset.wang@gmail.com>
2019-09-30 15:41:47 +0800

    Add: Generate Parentheses

--- problems/generate-parentheses/generate_parentheses.go
+++ problems/generate-parentheses/generate_parentheses.go
@@ -1,1 +1,19 @@
-package generate_parentheses
+package problem_22
+
+func generateParenthesis(n int) []string {
+	ans := make([]string, 0)
+	next("(", 1, 0, n, &ans)
+	return ans
+}
+
+func next(s string, l, r, n int, ans *[]string) {
+	if l == n && r == n {
+		*ans = append(*ans, s)
+	}
+	if l < n {
+		next(s+"(", l+1, r, n, ans)
+	}
+	if r < l {
+		next(s+")", l, r+1, n, ans)
+	}
+}
--- problems/generate-parentheses/generate_parentheses_test.go
+++ problems/generate-parentheses/generate_parentheses_test.go
@@ -1,1 +1,94 @@
-package generate_parentheses
+package problem_22
+
+import (
+	"reflect"
+	"testing"
+)
+
+type caseType struct {
+	input    int
+	expected []string
+}
+
+func TestGenerateParenthesis(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input: 3,
+			expected: []string{
+				"((()))",
+				"(()())",
+				"(())()",
+				"()(())",
+				"()()()",
+			},
+		},
+		{
+			input: 2,
+			expected: []string{
+				"(())",
+				"()()",
+			},
+		},
+		{
+			input:    1,
+			expected: []string{"()"},
+		},
+		{
+			input:    0,
+			expected: []string{},
+		},
+		{
+			input: 5,
+			expected: []string{
+				"((((()))))",
+				"(((()())))",
+				"(((())()))",
+				"(((()))())",
+				"(((())))()",
+				"((()(())))",
+				"((()()()))",
+				"((()())())",
+				"((()()))()",
+				"((())(()))",
+				"((())()())",
+				"((())())()",
+				"((()))(())",
+				"((()))()()",
+				"(()((())))",
+				"(()(()()))",
+				"(()(())())",
+				"(()(()))()",
+				"(()()(()))",
+				"(()()()())",
+				"(()()())()",
+				"(()())(())",
+				"(()())()()",
+				"(())((()))",
+				"(())(()())",
+				"(())(())()",
+				"(())()(())",
+				"(())()()()",
+				"()(((())))",
+				"()((()()))",
+				"()((())())",
+				"()((()))()",
+				"()(()(()))",
+				"()(()()())",
+				"()(()())()",
+				"()(())(())",
+				"()(())()()",
+				"()()((()))",
+				"()()(()())",
+				"()()(())()",
+				"()()()(())",
+				"()()()()()",
+			},
+		},
+	}
+	for _, tc := range tests {
+		output := generateParenthesis(tc.input)
+		if !reflect.DeepEqual(output, tc.expected) {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
