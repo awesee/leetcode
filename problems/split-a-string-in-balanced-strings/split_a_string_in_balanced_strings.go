e677d63bc52935976ae42e767a7cb52fb0299889
openset <openset.wang@gmail.com>
2019-11-05 12:29:31 +0800

    Add: Split a String in Balanced Strings

---
+++ problems/split-a-string-in-balanced-strings/split_a_string_in_balanced_strings.go
@@ -0,0 +1,16 @@
+package problem_1221
+
+func balancedStringSplit(s string) int {
+	ans, flag := 0, 0
+	for _, b := range s {
+		if b == 'L' {
+			flag--
+		} else {
+			flag++
+		}
+		if flag == 0 {
+			ans++
+		}
+	}
+	return ans
+}
