c49465b9c68bd0896a3bea9a11f714617c5f2bbd
openset <openset.wang@gmail.com>
2019-11-11 11:48:19 +0800

    Add: Kth Largest Element in an Array

--- problems/kth-largest-element-in-an-array/kth_largest_element_in_an_array.go
+++ problems/kth-largest-element-in-an-array/kth_largest_element_in_an_array.go
@@ -1,1 +1,8 @@
-package kth_largest_element_in_an_array
+package problem_215
+
+import "sort"
+
+func findKthLargest(nums []int, k int) int {
+	sort.Ints(nums)
+	return nums[len(nums)-k]
+}
--- problems/kth-largest-element-in-an-array/kth_largest_element_in_an_array_test.go
+++ problems/kth-largest-element-in-an-array/kth_largest_element_in_an_array_test.go
@@ -1,1 +1,29 @@
-package kth_largest_element_in_an_array
+package problem_215
+
+import "testing"
+
+type caseType struct {
+	input    []int
+	k        int
+	expected int
+}
+
+func TestFindKthLargest(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    []int{3, 2, 1, 5, 6, 4},
+			k:        2,
+			expected: 5,
+		}, {
+			input:    []int{3, 2, 3, 1, 2, 4, 5, 5, 6},
+			k:        4,
+			expected: 4,
+		},
+	}
+	for _, tc := range tests {
+		output := findKthLargest(tc.input, tc.k)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
