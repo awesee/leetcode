5fcca36f1792f364e012d06f8aa965547ebf443e
openset <openset.wang@gmail.com>
2019-11-12 11:33:11 +0800

    Add: Maximum Number of Balloons

---
+++ problems/maximum-number-of-balloons/maximum_number_of_balloons.go
@@ -0,0 +1,20 @@
+package problem_1189
+
+func maxNumberOfBalloons(text string) int {
+	ans, m := len(text), make(map[byte]int, 5)
+	for i := 0; i < ans; i++ {
+		switch text[i] {
+		case 'b', 'a', 'l', 'o', 'n':
+			m[text[i]]++
+		}
+	}
+	for _, c := range [...]byte{'b', 'a', 'l', 'o', 'n'} {
+		if c == 'l' || c == 'o' {
+			m[c] /= 2
+		}
+		if ans > m[c] {
+			ans = m[c]
+		}
+	}
+	return ans
+}
---
+++ problems/maximum-number-of-balloons/maximum_number_of_balloons_test.go
@@ -0,0 +1,35 @@
+package problem_1189
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected int
+}
+
+func TestMaxNumberOfBalloons(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "nlaebolko",
+			expected: 1,
+		},
+		{
+			input:    "loonbalxballpoon",
+			expected: 2,
+		},
+		{
+			input:    "leetcode",
+			expected: 0,
+		},
+		{
+			input:    "lloo",
+			expected: 0,
+		},
+	}
+	for _, tc := range tests {
+		output := maxNumberOfBalloons(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
