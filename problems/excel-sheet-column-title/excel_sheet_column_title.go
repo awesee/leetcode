a1f4780dc28eb97db06997cfa2cfaa0b6e682dc1
openset <openset.wang@gmail.com>
2019-11-12 14:05:26 +0800

    Add: Excel Sheet Column Title

--- problems/excel-sheet-column-title/excel_sheet_column_title.go
+++ problems/excel-sheet-column-title/excel_sheet_column_title.go
@@ -1,8 +1,19 @@
-package excel_sheet_column_title
+package problem_168
 
+// Solution 1: recursive
 func convertToTitle(n int) string {
 	if n--; n < 26 {
 		return string(n + 'A')
 	}
 	return convertToTitle(n/26) + string(n%26+'A')
+}
+
+// Solution 2: iteration
+func convertToTitle2(n int) string {
+	ans := ""
+	for ; n > 0; n /= 26 {
+		n--
+		ans = string(n%26+'A') + ans
+	}
+	return ans
 }
