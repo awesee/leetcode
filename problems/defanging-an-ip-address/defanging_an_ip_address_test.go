2b64b215fd5d4bf5e33c5f538f8537d286e34e9a
openset <openset.wang@gmail.com>
2019-11-12 11:48:42 +0800

    Add: Defanging an IP Address

---
+++ problems/defanging-an-ip-address/defanging_an_ip_address_test.go
@@ -0,0 +1,27 @@
+package problem_1108
+
+import "testing"
+
+type caseType struct {
+	input    string
+	expected string
+}
+
+func TestDefangIPaddr(t *testing.T) {
+	tests := [...]caseType{
+		{
+			input:    "1.1.1.1",
+			expected: "1[.]1[.]1[.]1",
+		},
+		{
+			input:    "255.100.50.0",
+			expected: "255[.]100[.]50[.]0",
+		},
+	}
+	for _, tc := range tests {
+		output := defangIPaddr(tc.input)
+		if output != tc.expected {
+			t.Fatalf("input: %v, output: %v, expected: %v", tc.input, output, tc.expected)
+		}
+	}
+}
