baa78033139bf8e5c5891cb1451370c2dfdc773c
openset <openset.wang@gmail.com>
2019-12-10 14:26:32 +0800

    simplified

--- tag/tags.js
+++ tag/tags.js
@@ -1,9 +1,9 @@
-tags = new Array();
+tags = [];
 document.querySelectorAll('#all-topic-tags>a').forEach(function (e) {
     tags.push({
         "Name": e.title || e.firstElementChild.innerText.trim(),
         "Slug": /tag\/(\S+?)\//.exec(e.href)[1],
-        "TranslatedName": e.innerText.trim().replace(/\n/g, '')
+        "TranslatedName": e.innerText.trim().replace(/\n/g, ''),
     });
 });
 newWindow = window.open('', 'frame_name');
