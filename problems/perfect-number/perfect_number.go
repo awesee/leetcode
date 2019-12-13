294aef34bf683dbf3aecc6fd93fc4b2154d91dd4
openset <openset.wang@gmail.com>
2019-12-13 10:45:15 +0800

    Add: Perfect Number

--- problems/perfect-number/perfect_number.go
+++ problems/perfect-number/perfect_number.go
@@ -1,1 +1,31 @@
 package problem507
+
+// Solution 1
+func checkPerfectNumber(num int) bool {
+	if num < 6 {
+		return false
+	}
+	sum, mid := 1, num
+	for i := 2; i < mid; i++ {
+		if num%i == 0 {
+			n := num / i
+			sum += i + n
+			if mid > n {
+				mid = n
+			}
+		}
+	}
+	return sum == num
+}
+
+// Solution 2
+func checkPerfectNumber2(num int) bool {
+	m := map[int]bool{
+		6:        true,
+		28:       true,
+		496:      true,
+		8128:     true,
+		33550336: true,
+	}
+	return m[num]
+}
