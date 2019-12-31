2c31b2a96ff028a8426d04b8fdae48cd40f8781d
Shuo <openset.wang@gmail.com>
2019-12-31 17:07:53 +0800

    Update: dayOfYear

--- problems/day-of-the-year/day_of_the_year.go
+++ problems/day-of-the-year/day_of_the_year.go
@@ -1,33 +1,8 @@
 package problem1154
 
-import "strconv"
-
-var daysBefore = [...]int{
-	0,
-	31,
-	31 + 28,
-	31 + 28 + 31,
-	31 + 28 + 31 + 30,
-	31 + 28 + 31 + 30 + 31,
-	31 + 28 + 31 + 30 + 31 + 30,
-	31 + 28 + 31 + 30 + 31 + 30 + 31,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
-}
+import "time"
 
 func dayOfYear(date string) int {
-	year, _ := strconv.Atoi(date[:4])
-	month, _ := strconv.Atoi(date[5:7])
-	day, _ := strconv.Atoi(date[8:])
-	day += daysBefore[month-1]
-	if month > 2 && isLeap(year) {
-		day++
-	}
-	return day
-}
-
-func isLeap(year int) bool {
-	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+	t, _ := time.Parse("2006-01-02", date)
+	return t.YearDay()
 }
