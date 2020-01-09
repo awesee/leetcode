af5e6780a319c25b90f1911a87009cc4c7416b6f
Shuo <openset.wang@gmail.com>
2020-01-09 10:14:29 +0800

    Add: Combine Two Tables

--- problems/combine-two-tables/combine_two_tables.sql
+++ problems/combine-two-tables/combine_two_tables.sql
@@ -1,1 +1,10 @@
 # Write your MySQL query statement below
+
+SELECT
+    Person.FirstName,
+    Person.LastName,
+    Address.City,
+    Address.State
+FROM
+    Person
+LEFT JOIN Address ON Person.PersonId = Address.PersonId;
