95129aaad345ac4322e9c67121ff46b544796a36
Shuo <openset.wang@gmail.com>
2020-01-16 10:45:29 +0800

    Add: Remove Nth Node From End of List

--- problems/remove-nth-node-from-end-of-list/remove_nth_node_from_end_of_list.go
+++ problems/remove-nth-node-from-end-of-list/remove_nth_node_from_end_of_list.go
@@ -1,1 +1,28 @@
 package problem19
+
+import "github.com/openset/leetcode/internal/kit"
+
+// ListNode - Definition for singly-linked list.
+type ListNode = kit.ListNode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	q := []*ListNode{head}
+	for head.Next != nil {
+		q = append(q, head.Next)
+		head = head.Next
+	}
+	i := len(q) - n
+	if i > 0 {
+		q[i-1].Next = q[i].Next
+	} else {
+		q[0] = q[i].Next
+	}
+	return q[0]
+}
