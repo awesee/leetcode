ed1b98cefe00060391ed7d4712f88e19e4928561
Shuo <openset.wang@gmail.com>
2020-04-22 09:53:23 +0800

    A: Path Sum III

--- problems/path-sum-iii/path_sum_iii.go
+++ problems/path-sum-iii/path_sum_iii.go
@@ -1,1 +1,33 @@
 package problem437
+
+import "github.com/openset/leetcode/internal/kit"
+
+// TreeNode - Definition for a binary tree node.
+type TreeNode = kit.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pathSum(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+	return findPath(root, sum) + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+}
+
+func findPath(root *TreeNode, sum int) int {
+	r := 0
+	if root == nil {
+		return 0
+	} else if root.Val == sum {
+		r++
+	}
+	r += findPath(root.Left, sum-root.Val)
+	r += findPath(root.Right, sum-root.Val)
+	return r
+}
--- problems/path-sum-iii/path_sum_iii_test.go
+++ problems/path-sum-iii/path_sum_iii_test.go
@@ -1,1 +1,30 @@
 package problem437
+
+import (
+	"testing"
+
+	"github.com/openset/leetcode/internal/kit"
+)
+
+type testType struct {
+	in   []int
+	sum  int
+	want int
+}
+
+func TestPathSum(t *testing.T) {
+	tests := [...]testType{
+		{
+			in:   []int{10, 5, -3, 3, 2, kit.NULL, 11, 3, -2, kit.NULL, 1},
+			sum:  8,
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		root := kit.SliceInt2TreeNode(tt.in)
+		got := pathSum(root, tt.sum)
+		if got != tt.want {
+			t.Fatalf("in: %v, got: %v, want: %v", tt.in, got, tt.want)
+		}
+	}
+}
