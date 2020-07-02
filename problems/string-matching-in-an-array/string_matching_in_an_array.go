76b5ee769c83701af60b64ee5290a35a1be8d90c
Shuo <openset.wang@gmail.com>
2020-07-02 14:33:13 +0800

    A: String Matching in an Array

--- problems/string-matching-in-an-array/string_matching_in_an_array.go
+++ problems/string-matching-in-an-array/string_matching_in_an_array.go
@@ -4,17 +4,12 @@ import "strings"
 
 func stringMatching(words []string) []string {
 	var ans []string
-	ok := func(str string) bool {
-		for _, word := range words {
-			if word != str && strings.Contains(word, str) {
-				return true
-			}
-		}
-		return false
-	}
 	for _, word := range words {
-		if ok(word) {
-			ans = append(ans, word)
+		for _, val := range words {
+			if val != word && strings.Contains(val, word) {
+				ans = append(ans, word)
+				break
+			}
 		}
 	}
 	return ans
